c560bafc36c77807c290ac442aceaf9d9dead526
Peter Andrews <peter.andrews@modolabs.com>
2016-02-01 11:30:48 -0500

    Add method to populate from a fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,8 @@
 import npc
 import pytest
 import os
+from tests.util import fixture_dir
+from distutils import dir_util
 
 @pytest.fixture(scope="module")
 def prefs():
@@ -16,14 +18,27 @@ class Campaign:
         self.chardir = tmpdir.mkdir(chardir)
 
     def get_character(self, filename):
+        """Get a file object for the given character filename"""
         return self.chardir.join(filename)
 
     def get_character_data(self, filename):
+        """Get the parsed data from the given character filename"""
         parseables = str(self.chardir.join(filename))
         return next(c for c in npc.parser.get_characters(search_paths=[parseables]))
 
     def get_absolute(self, filename):
+        """Get the fully qualified path to the given character filename"""
         return str(self.basedir.join(filename))
+
+    def populate_from_fixture_dir(self, fixture_path):
+        """Copy the files and folders from a fixture directory into the campaign
+        root.
+
+        The path given should be relative to the npc/tests/fixtures directory in
+        this package. It will be run through `fixture_dir` first.
+        """
+        srcpath = fixture_dir(fixture_path)
+        dir_util.copy_tree(srcpath, str(self.basedir))
 
 @pytest.fixture
 def campaign(tmpdir, request, prefs):
