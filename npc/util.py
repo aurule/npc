fe756e4a9b91b15996b986030652ab017be3cf93
Peter Andrews <peter.andrews@modolabs.com>
2016-06-30 14:32:39 -0400

    Add singleton metaclass

--- npc/util.py
+++ npc/util.py
@@ -42,3 +42,11 @@ def load_json(filename):
 def error(*args, **kwargs):
     """Print an error message to stderr."""
     print(*args, file=sysstderr, **kwargs)
+
+class Singleton(type):
+    """Metaclass for creating singleton classes."""
+    _instances = {}
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
