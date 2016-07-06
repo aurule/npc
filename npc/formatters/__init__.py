918df7244162581fb57f301ccf6a4bf4b96ce541
Peter Andrews <peter.andrews@modolabs.com>
2016-07-06 13:22:54 -0400

    Add docstring to formatters package

--- npc/formatters/__init__.py
+++ npc/formatters/__init__.py
@@ -1,1 +1,10 @@
+"""
+Character listing formatters
+
+These modules encapsulate the logic needed to create a character listing in
+various formats. Each module has a single `dump` entry point which accepts, at
+minimum, the characters to list and where to put them. Other args are available
+in each linter.
+"""
+
 from . import markdown, json
