c856dc0a08e9dd2b120a9ef7dcd3da34204b61f7
Peter Andrews <peter.andrews@modolabs.com>
2016-07-06 11:22:52 -0400

    Add error handling to json formatter

--- npc/formatters/json.py
+++ npc/formatters/json.py
@@ -1,9 +1,25 @@
 import json
 from .. import commands
 
-def dump(characters, f, meta):
+def dump(characters, out, meta=None):
+    """
+    Dump a json representation of all character data
+
+    Internally, it adds the keys in meta if present and then calls `json.dump`.
+
+    Args:
+        characters (list): Character dicts to dump
+        out (stream): Output stream to receive the json output
+        meta (dict): Metadata keys to add to the character data
+
+    Returns:
+        A commands.Result object. Openable will not be set.
+    """
     if meta:
         characters = [meta] + characters
 
-    json.dump(characters, f)
+    try:
+        json.dump(characters, out)
+    except Exception as e:
+        return commands.Result(False, errmsg=e, errcode=9)
     return commands.Result(True)
