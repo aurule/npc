5d01c58aef7f101531ecc7a44a83d225fa2fdcc8
Peter Andrews <peter.andrews@modolabs.com>
2016-07-06 11:16:44 -0400

    Add docstring to linters package

--- npc/linters/__init__.py
+++ npc/linters/__init__.py
@@ -1,1 +1,13 @@
+"""
+Linters for verifying the correctness of certain character types
+
+The `commands.lint` function can lint all basic files, but special character
+types sometimes need extra checks. The linters in this package encapsulate that
+logic.
+
+All linter packages have a single main entry point `lint` which accepts a
+character information dict. Various keyword arguments are used for options and
+supporting data.
+"""
+
 from . import changeling
