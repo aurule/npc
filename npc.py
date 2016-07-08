311989f227278ce9d433ab2852b50ffcdee188d6
Peter Andrews <peter.andrews@modolabs.com>
2016-07-08 17:06:24 -0400

    Add module docstring and reorder imports

--- npc.py
+++ npc.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3.5
 
-import npc
+"""
+Executable entry point for the NPC CLI interface.
+"""
+
 import sys
+import npc
 
 if __name__ == '__main__':
     npc.cli(sys.argv[1:])
