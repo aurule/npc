fc2c29d1bc0c823af89ebd795fdde1e0f619f026
Peter Andrews <peter.andrews@modolabs.com>
2017-01-06 15:56:24 -0500

    Add a startup error box

--- npc/gui/__init__.py
+++ npc/gui/__init__.py
@@ -8,13 +8,9 @@ from tkinter.ttk import *
 from .. import commands
 from .. import main
 
+
 def run():
     root = Tk()
-
-    # set default window properties
-    root.title("NPC")
-    root.positionfrom('user')
-    root.minsize(width=200, height=300)
 
     # set up the widgets
     npc_app = NPCApp(root)
@@ -24,11 +20,23 @@ def run():
     root.destroy()
 
 def startup_error(message):
-    raise NotImplementedError
-    # TODO create a window that shows the error text with a quit button
+    root = Tk()
+
+    # set up the widgets
+    error_window = StartupError(root, message)
+
+    # run and done
+    root.mainloop()
+    root.destroy()
 
 class NPCApp:
     def __init__(self, master):
+        self.master = master
+
+        master.title("NPC")
+        master.positionfrom('user')
+        master.minsize(width=200, height=300)
+
         frame = Frame(master)
         frame.pack()
 
@@ -40,3 +48,17 @@ class NPCApp:
 
     def say_hi(self):
         print("hi there, everyone!")
+
+class StartupError:
+    def __init__(self, master, message):
+        master.title("NPC - Startup Error!")
+        master.positionfrom('user')
+
+        frame = Frame(master)
+        frame.pack()
+
+        self.error_text = Message(frame, text=message)
+        self.error_text.pack()
+
+        self.quit_button = Button(frame, text="Quit", command=frame.quit)
+        self.quit_button.pack()
