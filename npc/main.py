2da2f63becd3ff21802a64dddaba0004e333fbf3
Peter Andrews <peter.andrews@modolabs.com>
2017-01-06 16:26:29 -0500

    Add version information
    The version number here is inaccurate and will definitely change.

--- npc/main.py
+++ npc/main.py
@@ -12,6 +12,9 @@ from subprocess import run
 from . import commands, util, settings
 from . import gui as gui_core
 
+VERSION = "1.0.0"
+"""str: Current code version"""
+
 def cli(argv):
     """
     Run the command-line interface
@@ -173,6 +176,7 @@ def _make_parser():
     parser.add_argument('-b', '--batch', action='store_true', default=False, help="Do not open any newly created files")
     parser.add_argument('--campaign', default='auto', help="Use the campaign files in a different directory", metavar='DIR')
     parser.add_argument('--debug', action='store_true', default=False, help="Show all error messages, not just important ones")
+    parser.add_argument('--version', action='version', version=VERSION)
     subparsers = parser.add_subparsers(title='Subcommands', description="Commands that can be run on the current campaign. See `%(prog)s <command> -h` to get help with individual commands.")
 
     # Subcommand to create the basic directories
