aa70a599edc1b0b78be67f25f4e99eb7ff7fc3c9
Peter <peter.andrews3@icloud.com>
2017-01-07 00:33:14 -0500

    Properly show error messages

--- npc/gui/__init__.py
+++ npc/gui/__init__.py
@@ -10,18 +10,58 @@ from subprocess import run
 from .uis import *
 from .. import commands, main, settings
 
+def start():
+    """Main entry point for the GUI"""
+    app = QtWidgets.QApplication(sys.argv)
+    window = QtWidgets.QMainWindow()
+
+    prog = MainWindow(window)
+
+    window.show()
+    sys.exit(app.exec_())
+
+def startup_error(message):
+    """
+    Show an error that happened during startup
+
+    This is used for errors that occur before the main UI has
+    been created. Other errors should use _show_error.
+
+    Args:
+        message (str): Message text to display
+    """
+    app = QtWidgets.QApplication([])
+    errorbox = QtWidgets.QMessageBox.critical(
+        None,
+        'Could not start NPC',
+        message,
+        QtWidgets.QMessageBox.Ok)
+
+def _show_error(parent, title, message):
+    """
+    Helper to show an modal error window
+
+    Args:
+        parent (object): Parent window for the modal. This
+            window will be disabled while the modal is visible.
+        title (str): Title for the error window
+        message (str): Message text to display
+    """
+    errorbox = QtWidgets.QMessageBox.warning(parent, title, message, QtWidgets.QMessageBox.Ok)
+
 class MainWindow(Ui_MainWindow):
     def __init__(self, window):
         self.prefs = settings.InternalSettings()
         Ui_MainWindow.__init__(self)
 
         # main window setup
+        self.window = window
         self.setupUi(window)
         self.force_titles()
 
         # about dialog
         self.about_dialog = QtWidgets.QDialog(window)
-        about_dialog_content = About(self.about_dialog)
+        about_dialog_content = AboutDialog(self.about_dialog)
         self.actionAbout.triggered.connect(self.about_dialog.open)
 
         # commands setup
@@ -35,36 +75,27 @@ class MainWindow(Ui_MainWindow):
         self.menuFile.setTitle(_translate("MainWindow", "&File"))
         self.menuSettings.setTitle(_translate("MainWindow", "&Settings"))
 
+    @contextmanager
+    def safe_command(self, command):
+        try:
+            yield command
+        except AttributeError as err:
+            _show_error(self.window, 'Command failed', err)
+
     def run_user_settings(self):
-        with safe_command(commands.open_settings) as command:
+        with self.safe_command(commands.open_settings) as command:
             result = command('campaign', show_defaults=True)
 
             if not result.success:
-                messagebox.showerror("Error!", result)
+                _show_error(self.window, 'Could not open user settings', result)
 
             run([self.prefs.get("editor")] + result.openable)
 
     def quit(self):
         QtCore.QCoreApplication.instance().quit()
 
-@contextmanager
-def safe_command(command):
-    try:
-        yield command
-    except AttributeError as err:
-        messagebox.showerror("Error!", err)
-
-class About(Ui_AboutDialog):
+class AboutDialog(Ui_AboutDialog):
     def __init__(self, dialog):
         Ui_AboutDialog.__init__(self)
         self.setupUi(dialog)
         self.labelVersion.setText("Version {0}".format(main.VERSION))
-
-def start():
-    app = QtWidgets.QApplication(sys.argv)
-    window = QtWidgets.QMainWindow()
-
-    prog = MainWindow(window)
-
-    window.show()
-    sys.exit(app.exec_())
--- npc/gui/uis/main_window.py
+++ npc/gui/uis/main_window.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'npc/gui/main.ui'
+# Form implementation generated from reading ui file 'npc/gui/uis/main_window.ui'
 #
 # Created by: PyQt5 UI code generator 5.7.1
 #
