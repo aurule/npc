dab9a3af8ad9ae5f8c22b9d9f9d0fe7388a24a22
Peter Andrews <peter.andrews@modolabs.com>
2017-01-23 14:25:57 -0500

    Rename init dialog ui file

--- npc/gui/uis/__init__.py
+++ npc/gui/uis/__init__.py
@@ -4,5 +4,5 @@ Package for UI definitions
 
 from .main_window import Ui_MainWindow
 from .about import Ui_AboutDialog
-from .init import Ui_InitDialog
+from .init_dialog import Ui_InitDialog
 from .new_character import Ui_NewCharacterDialog
