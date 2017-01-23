46df43a5dd94a1dd2304a4f3ca8c29cd28daefdf
Peter Andrews <peter.andrews@modolabs.com>
2017-01-23 14:52:35 -0500

    Refactor main window into its own file

--- npc/gui/uis/__init__.py
+++ npc/gui/uis/__init__.py
@@ -1,8 +1,3 @@
 """
 Package for UI definitions
 """
-
-from .main_window import Ui_MainWindow
-from .about import Ui_AboutDialog
-from .init_dialog import Ui_InitDialog
-from .new_character import Ui_NewCharacterDialog
