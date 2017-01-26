e4366181885fcd922551e6d2150e18dda6c4447b
Peter Andrews <peter.andrews@modolabs.com>
2017-01-26 14:12:15 -0500

    Add many more character tests

--- tests/test_character.py
+++ tests/test_character.py
@@ -1,4 +1,10 @@
 import npc
+import pytest
+
+def test_append_rank():
+    char = npc.Character()
+    char.append_rank("Knights of the Round Table", "Dancer")
+    assert char["rank"]["Knights of the Round Table"] == ["Dancer"]
 
 class TestCreation:
     """Test different instantiation behaviors"""
@@ -54,18 +60,94 @@ class TestAppend:
         char.append("description", " baby, you called")
         assert char["description"] == "Hello hello baby, you called"
 
-def test_append_rank():
-    char = npc.Character()
-    char.append_rank("Knights of the Round Table", "Dancer")
-    assert char["rank"]["Knights of the Round Table"] == ["Dancer"]
+class TestTypeKey:
+    def test_lowercase(self):
+        char = npc.Character(type=['Fish', 'Great Ape'])
+        assert char.type_key == 'fish'
+
+    def test_empty(self):
+        char = npc.Character()
+        assert char.type_key is None
+
+class TestGetFirst:
+    def test_normal(self):
+        char = npc.Character(name=["hello", "goodbye"])
+        assert char.get_first('name') == 'hello'
+
+    @pytest.mark.parametrize('keyname', npc.Character.STRING_TAGS)
+    def test_string_tags(self, keyname):
+        char = npc.Character()
+        char.append(keyname, "hello")
+        char.append(keyname, " friend")
+        assert char.get_first(keyname) == "hello friend"
+
+    def test_missing(self):
+        char = npc.Character()
+        assert char.get_first('nope', 'negative') == 'negative'
+        assert char.get_first('nope') is None
+
+class TestGetRemaining:
+    def test_normal(self):
+        char = npc.Character(name=["hello", "goodbye"])
+        assert char.get_remaining('name') == ['goodbye']
+
+    @pytest.mark.parametrize('keyname', npc.Character.STRING_TAGS)
+    def test_string_tags(self, keyname):
+        char = npc.Character()
+        char.append(keyname, "hello")
+        char.append(keyname, " friend")
+        assert char.get_remaining(keyname) == "hello friend"
+
+    def test_missing(self):
+        char = npc.Character()
+        assert char.get_remaining('nope') == []
+
+class TestHasItems:
+    @pytest.mark.parametrize('limit', [(1, True), (5, True), (10, True), (11, False)])
+    def test_thresholds(self, limit):
+        char = npc.Character()
+        for thing in range(0, 10):
+            char.append('things', thing)
+        assert char.has_items('things', limit[0]) == limit[1]
+
+    def test_bad_threshold(self):
+        char = npc.Character()
+        with pytest.raises(npc.util.OutOfBoundsError) as ex:
+            char.has_items('things', 0)
+
+class TestCopyAndAlter:
+    def titleize(self, text):
+        return text.title()
+
+    def test_single(self):
+        char = npc.Character()
+        char.append('snoot', 'booped')
+        new_char = char.copy_and_alter(self.titleize)
+        assert new_char.get('snoot') == ['Booped']
+
+    def test_multiple(self):
+        char = npc.Character()
+        char.append('hands', 'raised')
+        char.append('hands', 'jazzy')
+        new_char = char.copy_and_alter(self.titleize)
+        assert new_char.get('hands') == ['Raised', 'Jazzy']
+
+    @pytest.mark.parametrize('keyname', npc.Character.STRING_TAGS)
+    def test_string(self, keyname):
+        char = npc.Character()
+        char.append(keyname, 'hello hello')
+        new_char = char.copy_and_alter(self.titleize)
+        assert new_char.get(keyname) == "Hello Hello"
+
+    def test_rank(self):
+        char = npc.Character()
+        char.append_rank('restaurant', 'chef')
+        char.append_rank('restaurant', 'newb')
+        new_char = char.copy_and_alter(self.titleize)
+        assert new_char.get('rank') == {'restaurant': ['Chef', 'Newb']}
 
 # tests to do:
-# valid
-# typekey
-# get first, and with string tags
-# get remaining, and with string tags
 # validation
 # changeling validation
-# has items
-# copy and alter
+# valid
 # build header
