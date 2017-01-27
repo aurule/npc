484cd3bf98c1ddb91e8f2235a9e6d3c12dc239c6
Peter Andrews <peter.andrews@modolabs.com>
2017-01-27 16:36:39 -0500

    Plan out util tests

--- tests/test_util.py
+++ tests/test_util.py
@@ -1,4 +1,5 @@
 import npc
+from npc.util import Result
 import pytest
 from tests.util import fixture_dir
 
@@ -8,9 +9,29 @@ from tests.util import fixture_dir
 # flatten(['some text', 5, ['yet more']]) should yield ['some text', 5, 'yet more']
 # find campaign root returns folder containing .npc or current directory if not found
 
+# json loading
+#   ignores comments // and /*..*/
+#   on bad syntax, reports a nice error string in err.nicemsg
+#   if cannot load, includes nice error string in err.nicemsg
+#
+# error prints to stderr
+#
+# flatten handles mixed iterable types: flatten(['some text', 5, ['yet more']]) should yield ['some text', 5, 'yet more']
+#
+# find_campaign_root returns folder containing .npc or current directory if not found
+
 def test_open_files(prefs):
     override_path = fixture_dir('util', 'open_files', 'settings-echo.json')
     prefs.load_more(override_path)
     result = npc.util.open_files('not a real path', 'seriously, no', prefs=prefs)
     assert 'not a real path' in result.args
     assert 'seriously, no' in result.args
+
+class TestResult:
+    def test_str_success(self):
+        result = Result(True)
+        assert str(Result) == "Success"
+
+# Result
+#   __str__ gives "Success" or self.errmsg
+#   __bool__ gives self.success
