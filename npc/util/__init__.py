3833eac17dcab5de4d3bfa8527c7d0947a4a4749
Peter Andrews <peter.andrews@modolabs.com>
2017-09-20 15:38:08 -0400

    Move result to its own module

--- npc/util/__init__.py
+++ npc/util/__init__.py
@@ -3,3 +3,4 @@ Helper functions shared between the other modules
 """
 
 from .core import *
+from .result import *
--- npc/util/core.py
+++ npc/util/core.py
@@ -128,46 +128,5 @@ class Singleton(type):
             cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
         return cls._instances[cls]
 
-class Result:
-    """
-    Data about the result of a subcommand
-
-    Attributes:
-        success (bool): Whether the subcommand ran correctly
-        openable (list): Paths to files which were changed by or are relevant to
-            the subcommand
-        errcode (int): Error code indicating the type of error encountered.
-
-            Error codes:
-            0 -- Everything's fine
-            1 -- Tried to create a file that already exists
-            2 -- Latest plot and session files have different numbers
-            3 -- Feature is not yet implemented
-            4 -- Filesystem error
-            5 -- Unrecognized format
-            6 -- Invalid option
-            7 -- Unrecognized template
-            8 -- Missing required file
-        errmsg (str): Human-readable error message. Will be displayed to the
-            user.
-        printables (list[str]): List of strings that detail changes made. Safe to
-            leave blank.
-    """
-    def __init__(self, success, **kwargs):
-        super(Result, self).__init__()
-        self.success = success
-        self.openable = kwargs.get('openable', [])
-        self.errcode = kwargs.get('errcode', 0)
-        self.errmsg = kwargs.get('errmsg', '')
-        self.printables = kwargs.get('printables', [])
-
-    def __str__(self):
-        if self.success:
-            return "Success"
-        return self.errmsg
-
-    def __bool__(self):
-        return self.success
-
 class OutOfBoundsError(ValueError):
     """Raise when a function input is outside of permitted bounds"""
