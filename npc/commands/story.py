e48eda23120856f215e3716e6b3e9b0c16a9e0f1
Peter Andrews <pmandrews@gmail.com>
2017-09-27 12:14:03 -0400

    Refactor helper function

--- npc/commands/story.py
+++ npc/commands/story.py
@@ -9,12 +9,17 @@ from shutil import copy as shcopy
 from npc import settings
 from npc.util import result
 
+PLOT_REGEX = re.compile(r'(?i)^plot (?P<number>\d+)$')
+SESSION_REGEX = re.compile(r'(?i)^session (?P<number>\d+)$')
+
 def session(**kwargs):
     """
     Create the files for a new game session.
 
     Finds the plot and session log files for the last session, copies the plot,
-    and creates a new empty session log.
+    and creates a new empty session log. If the latest plot file is ahead of
+    the latest session, a new plot file will *not* be created. Likewise if the
+    latest session file is ahead, a new session file will *not* be created.
 
     Args:
         prefs (Settings): Settings object to use. Uses internal settings by
@@ -31,33 +36,8 @@ def session(**kwargs):
     if not (path.exists(plot_path) and path.exists(session_path)):
         return result.FSError(errmsg="Cannot access paths '{}' and/or '{}'".format(plot_path, session_path))
 
-    plot_re = re.compile(r'(?i)^plot (?P<number>\d+)$')
-    session_re = re.compile(r'(?i)^session (?P<number>\d+)$')
-
-    def get_latest(target_path, target_regex):
-        """Get the "latest" file in target_path that matches target_regex"""
-        buncha_files = [f.name for f in scandir(target_path) if f.is_file() and target_regex.match(path.splitext(f.name)[0])]
-        try:
-            latest_file = max(buncha_files, key=lambda f: re.split(r'\s', f)[1])
-            (bare_name, bare_ext) = path.splitext(latest_file)
-            file_match = target_regex.match(bare_name)
-            file_number = int(file_match.group('number'))
-        except ValueError:
-            latest_file = ''
-            bare_ext = '.md'
-            file_number = 0
-
-        return {
-            'name': latest_file,
-            'ext': bare_ext,
-            'number': file_number,
-            'path': path.join(target_path, latest_file),
-            'exists': file_number > 0
-        }
-
-    # find latest plot and session file
-    latest_plot = get_latest(plot_path, plot_re)
-    latest_session = get_latest(session_path, session_re)
+    latest_plot = latest_file_info(plot_path, PLOT_REGEX)
+    latest_session = latest_file_info(session_path, SESSION_REGEX)
 
     new_number = min(latest_plot['number'], latest_session['number']) + 1
 
@@ -99,3 +79,38 @@ def session(**kwargs):
         openable.append(new_plot_path)
 
     return result.Success(openable=openable)
+
+def latest(thingtype, **kwargs):
+    """
+    Open the latest plot and/or session file
+
+    Args:
+        prefs (Settings): Settings object to use. Uses internal settings by
+            default.
+
+    Returns:
+        Result object. Openable will contain the path(s) to the requested file(s).
+    """
+    prefs = kwargs.get('prefs', settings.InternalSettings())
+    plot_path = prefs.get('paths.plot')
+    session_path = prefs.get('paths.session')
+
+    if not (path.exists(plot_path) and path.exists(session_path)):
+        return result.FSError(errmsg="Cannot access paths '{}' and/or '{}'".format(plot_path, session_path))
+
+    latest_plot = latest_file_info(plot_path, PLOT_REGEX)
+    latest_session = latest_file_info(session_path, SESSION_REGEX)
+    if thingtype == 'both':
+        openable = [latest_plot['path'], latest_session['path']]
+    elif thingtype == 'session':
+        openable = [latest_session['path']]
+    elif thingtype == 'plot':
+        openable = [latest_plot['path']]
+    else:
+        return result.OptionError(errmsg="Unrecognized type '{}'".format(thingtype))
+
+    return result.Success(openable=openable)
+
+
+def latest_file_info(target_path, target_regex):
+    pass
