9c19dd782361c08f7d3a3d51b0f6cbbabde33609
Peter Andrews <peter.andrews@modolabs.com>
2017-10-31 16:53:29 -0400

    Redo most changeling tests

--- tests/linters/test_lint_changeling.py
+++ tests/linters/test_lint_changeling.py
@@ -5,63 +5,106 @@ import pytest
 import os
 from tests.util import fixture_dir
 
-@pytest.fixture
-def lint_output():
-    def do_lint(charname, strict=False):
-        search = fixture_dir('linter', 'characters', 'Changelings', charname)
-        result = npc.commands.lint(search, strict=strict)
-        return "\n".join(result.printables)
-    return do_lint
+def lint_changeling(filename, **kwargs):
+    prefs = npc.settings.Settings()
+    char_file = fixture_dir('linter', 'changeling', filename)
+    character = npc.parser.parse_character(char_file)
+    problems = npc.linters.changeling.lint(character, sk_data=prefs.get('changeling'), **kwargs)
+    return '/'.join(problems)
 
-@pytest.mark.parametrize('charname', ['No Kith.nwod', 'No Kith Also.nwod'])
-def test_seeming_present(lint_output, charname):
-    assert "Missing kith" in lint_output(charname)
+class TestSeeming:
+    def test_known_seeming(self):
+        problems = lint_changeling('Known Seeming.nwod')
+        assert "Unrecognized @seeming" not in problems
 
-def test_seeming_info_present(lint_output):
-    assert "Missing notes for Seeming" in lint_output('No Info.nwod')
+    def test_unknown_seeming(self):
+        problems = lint_changeling('Unknown Seeming.nwod')
+        assert "Unrecognized @seeming" in problems
 
-def test_seeming_info_correct(lint_output):
-    assert "Incorrect notes for Seeming" in lint_output('Bad Info.nwod')
+    def test_seeming_info_present(self):
+        problems = lint_changeling('Has Info.nwod')
+        assert "Seeming stats do not match @seeming tags" not in problems
 
-def test_kith_present(lint_output):
-    assert "Missing seeming" in lint_output('No Seeming.nwod')
+    def test_seeming_info_not_present(self):
+        problems = lint_changeling('No Info.nwod')
+        assert "Seeming stats do not match @seeming tags" in problems
 
-def test_kith_info_present(lint_output):
-    assert "Missing notes for Kith" in lint_output('No Info.nwod')
+    def test_seeming_info_correct(self):
+        problems = lint_changeling('Has Info.nwod')
+        assert "Incorrect notes for Seeming" not in problems
 
-def test_kith_info_correct(lint_output):
-    assert "Incorrect notes for Kith" in lint_output('Bad Info.nwod')
+    def test_seeming_info_incorrect(self):
+        problems = lint_changeling('Bad Info.nwod')
+        assert "Incorrect notes for Seeming" in problems
 
-def test_unknown_kith(lint_output):
-    assert "Unrecognized @kith" in lint_output('Unknown Kith.nwod')
+class TestKith:
+    def test_known_kith(self):
+        problems = lint_changeling('Known Kith.nwod')
+        assert "Unrecognized @kith" not in problems
 
-def test_multiple_courts(lint_output):
-    assert "Multiple courts" in lint_output('Many Courts.nwod')
+    def test_unknown_kith(self):
+        problems = lint_changeling('Unknown Kith.nwod')
+        assert "Unrecognized @kith" in problems
 
-def test_multiple_motleys(lint_output):
-    assert "Multiple motleys" in lint_output('Many Motleys.nwod')
+    def test_kith_info_present(self):
+        problems = lint_changeling('Has Info.nwod')
+        assert "Kith stats do not match @kith tags" not in problems
 
-def test_multiple_mantles(lint_output):
-    assert "Multiple mantle" in lint_output('Many Mantles.nwod')
+    def test_kith_info_not_present(self):
+        problems = lint_changeling('No Info.nwod')
+        assert "Kith stats do not match @kith tags" in problems
 
-def test_wrong_mantle(lint_output):
-    assert "Court mantle 'Winter' does not match court tag 'Summer'" in lint_output('Wrong Mantle.nwod')
+    def test_kith_info_correct(self):
+        problems = lint_changeling('Has Info.nwod')
+        assert "Incorrect notes for Kith" not in problems
 
-def test_goodwill_vs_court(lint_output):
-    assert "Court goodwill listed for court tag 'Winter'" in lint_output('Goodwill.nwod')
+    def test_kith_info_incorrect(self):
+        problems = lint_changeling('Bad Info.nwod')
+        assert "Incorrect notes for Kith" in problems
 
-def test_goodwill_vs_mantle(lint_output):
-    assert "Court goodwill listed for court mantle 'Winter'" in lint_output('Goodwill.nwod')
+class TestMantle:
+    def test_allows_one_mantle(self):
+        problems = lint_changeling('One Mantle.nwod')
+        assert "Multiple mantle" not in problems
 
-class TestStrictChangelingLinting:
-    def test_court_no_mantle(self, lint_output):
-        assert "No mantle for court 'Winter'" in lint_output('No Mantle.nwod', strict=True)
+    def test_multiple_mantles(self):
+        problems = lint_changeling('Many Mantles.nwod')
+        assert "Multiple mantle" in problems
 
-    def test_unseen_sense(self, lint_output):
-        assert "Changelings cannot have the Unseen Sense merit" in lint_output('Unseen Sense.nwod', strict=True)
+    def test_mantle_merit_matches_tag(self):
+        problems = lint_changeling('Right Mantle.nwod')
+        assert "Court mantle 'Winter' does not match" not in problems
 
-    def test_virtue(self, lint_output):
-        assert "Missing virtue" in lint_output('No Virtue.nwod', strict=True)
+    def test_mantle_merit_does_not_match_tag(self):
+        problems = lint_changeling('Wrong Mantle.nwod')
+        assert "Court mantle 'Winter' does not match" in problems
 
-    def test_vice(self, lint_output):
-        assert "Missing vice" in lint_output('No Vice.nwod', strict=True)
+class TestGoodwill:
+    def test_goodwill_for_other_court_tag(self):
+        problems = lint_changeling('Goodwill for Other.nwod')
+        assert "Court goodwill listed for court tag" not in problems
+
+    def test_goodwill_for_same_court_tag(self):
+        problems = lint_changeling('Goodwill for Same.nwod')
+        assert "Court goodwill listed for court tag" in problems
+
+    def test_goodwill_for_other_court_merit(self):
+        problems = lint_changeling('Goodwill for Other.nwod')
+        assert "Court goodwill listed for court mantle" not in problems
+
+    def test_goodwill_for_same_court_merit(self):
+        problems = lint_changeling('Goodwill for Same.nwod')
+        assert "Court goodwill listed for court mantle" in problems
+
+# class TestStrictChangelingLinting:
+#     def test_court_no_mantle(self, lint_output):
+#         assert "No mantle for court 'Winter'" in lint_output('No Mantle.nwod', strict=True)
+
+#     def test_unseen_sense(self, lint_output):
+#         assert "Changelings cannot have the Unseen Sense merit" in lint_output('Unseen Sense.nwod', strict=True)
+
+#     def test_virtue(self, lint_output):
+#         assert "Missing virtue" in lint_output('No Virtue.nwod', strict=True)
+
+#     def test_vice(self, lint_output):
+#         assert "Missing vice" in lint_output('No Vice.nwod', strict=True)
