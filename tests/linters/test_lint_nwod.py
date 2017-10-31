1d476a4551dbf47bd4859399e607158a2c15de8e
Peter Andrews <peter.andrews@modolabs.com>
2017-10-31 16:06:12 -0400

    Add positive vice and virtue tests

--- tests/linters/test_lint_nwod.py
+++ tests/linters/test_lint_nwod.py
@@ -4,12 +4,24 @@ import os
 from tests.util import fixture_dir
 
 def test_has_virtue():
+    char_file = fixture_dir('linter', 'nwod', 'Has Virtue.nwod')
+    with open(char_file, 'r') as char_file:
+        problems = npc.linters.nwod.lint_vice_virtue(char_file.read())
+        assert 'Missing virtue' not in problems
+
+def test_missing_virtue():
     char_file = fixture_dir('linter', 'nwod', 'Gotta Nada.nwod')
     with open(char_file, 'r') as char_file:
         problems = npc.linters.nwod.lint_vice_virtue(char_file.read())
         assert 'Missing virtue' in problems
 
 def test_has_vice():
+    char_file = fixture_dir('linter', 'nwod', 'Has Vice.nwod')
+    with open(char_file, 'r') as char_file:
+        problems = npc.linters.nwod.lint_vice_virtue(char_file.read())
+        assert 'Missing virtue' not in problems
+
+def test_missing_vice():
     char_file = fixture_dir('linter', 'nwod', 'Gotta Nada.nwod')
     with open(char_file, 'r') as char_file:
         problems = npc.linters.nwod.lint_vice_virtue(char_file.read())
