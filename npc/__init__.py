589be5a0c41fff12d923dfe9c2bba160c95d5cac
Peter Andrews <peter.andrews@modolabs.com>
2017-11-16 11:13:42 -0500

    Remove unused imports

--- npc/__init__.py
+++ npc/__init__.py
@@ -2,5 +2,5 @@
 Package for reading and manipulating campaign information.
 """
 
-from . import cli, gui, commands, settings, __version__
+from . import commands, settings, __version__
 from .character import Character
