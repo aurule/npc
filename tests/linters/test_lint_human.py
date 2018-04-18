041ba2df191961376f0beecca061fda7e44decae
Peter Andrews <peter.andrews@modolabs.com>
2018-04-18 14:03:37 -0400

    Refactor out common test setups

--- tests/linters/test_lint_human.py
+++ tests/linters/test_lint_human.py
@@ -6,6 +6,13 @@ import os
 import functools
 from tests.util import fixture_dir
 
+def lint_human(filename, **kwargs):
+    prefs = npc.settings.Settings()
+    char_file = fixture_dir('linter', 'human', filename)
+    character = npc.parser.parse_character(char_file)
+    problems = npc.linters.human.lint(character, **kwargs)
+    return problems
+
 def test_requires_path():
     character = npc.Character()
     problems = npc.linters.human.lint(character)
@@ -17,9 +24,7 @@ class TestViceAndVirtue:
                              (False, False),
                              ])
     def test_has_virtue(self, prefs, strict, is_problem):
-        char_file = fixture_dir('linter', 'human', 'Has Virtue.nwod')
-        character = npc.parser.parse_character(char_file)
-        problems = npc.linters.human.lint(character, strict=strict)
+        problems = lint_human('Has Virtue.nwod', strict=strict)
         assert ('Missing virtue' in problems) == is_problem
 
     @pytest.mark.parametrize("strict,is_problem", [
@@ -27,9 +32,7 @@ class TestViceAndVirtue:
                              (False, False),
                              ])
     def test_missing_virtue(self, prefs, strict, is_problem):
-        char_file = fixture_dir('linter', 'human', 'Gotta Nada.nwod')
-        character = npc.parser.parse_character(char_file)
-        problems = npc.linters.human.lint(character, strict=strict)
+        problems = lint_human('Gotta Nada.nwod', strict=strict)
         assert ('Missing virtue' in problems) == is_problem
 
     @pytest.mark.parametrize("strict,is_problem", [
@@ -37,9 +40,7 @@ class TestViceAndVirtue:
                              (False, False),
                              ])
     def test_has_vice(self, prefs, strict, is_problem):
-        char_file = fixture_dir('linter', 'human', 'Has Vice.nwod')
-        character = npc.parser.parse_character(char_file)
-        problems = npc.linters.human.lint(character, strict=strict)
+        problems = lint_human('Has Vice.nwod', strict=strict)
         assert ('Missing vice' in problems) == is_problem
 
     @pytest.mark.parametrize("strict,is_problem", [
@@ -47,7 +48,5 @@ class TestViceAndVirtue:
                              (False, False),
                              ])
     def test_missing_vice(self, prefs, strict, is_problem):
-        char_file = fixture_dir('linter', 'human', 'Gotta Nada.nwod')
-        character = npc.parser.parse_character(char_file)
-        problems = npc.linters.human.lint(character, strict=strict)
+        problems = lint_human('Gotta Nada.nwod', strict=strict)
         assert ('Missing vice' in problems) == is_problem
--- tests/linters/test_lint_werewolf.py
+++ tests/linters/test_lint_werewolf.py
@@ -6,6 +6,13 @@ import os
 import functools
 from tests.util import fixture_dir
 
+def lint_werewolf(filename, **kwargs):
+    prefs = npc.settings.Settings()
+    char_file = fixture_dir('linter', 'werewolf', filename)
+    character = npc.parser.parse_character(char_file)
+    problems = npc.linters.werewolf.lint(character, prefs=prefs, **kwargs)
+    return problems
+
 def test_requires_path(prefs):
     character = npc.Character()
     problems = npc.linters.werewolf.lint(character, prefs=prefs)
@@ -16,64 +23,48 @@ class TestViceAndVirtue:
                              (True, False),
                              (False, False),
                              ])
-    def test_has_virtue(self, prefs, strict, is_problem):
-        char_file = fixture_dir('linter', 'werewolf', 'Has Virtue.nwod')
-        character = npc.parser.parse_character(char_file)
-        problems = npc.linters.werewolf.lint(character, strict=strict, prefs=prefs)
+    def test_has_virtue(self, strict, is_problem):
+        problems = lint_werewolf('Has Virtue.nwod', strict=strict)
         assert ('Missing virtue' in problems) == is_problem
 
     @pytest.mark.parametrize("strict,is_problem", [
                              (True, True),
                              (False, False),
                              ])
-    def test_missing_virtue(self, prefs, strict, is_problem):
-        char_file = fixture_dir('linter', 'werewolf', 'Gotta Nada.nwod')
-        character = npc.parser.parse_character(char_file)
-        problems = npc.linters.werewolf.lint(character, strict=strict, prefs=prefs)
+    def test_missing_virtue(self, strict, is_problem):
+        problems = lint_werewolf('Gotta Nada.nwod', strict=strict)
         assert ('Missing virtue' in problems) == is_problem
 
     @pytest.mark.parametrize("strict,is_problem", [
                              (True, False),
                              (False, False),
                              ])
-    def test_has_vice(self, prefs, strict, is_problem):
-        char_file = fixture_dir('linter', 'werewolf', 'Has Vice.nwod')
-        character = npc.parser.parse_character(char_file)
-        problems = npc.linters.werewolf.lint(character, strict=strict, prefs=prefs)
+    def test_has_vice(self, strict, is_problem):
+        problems = lint_werewolf('Has Vice.nwod', strict=strict)
         assert ('Missing vice' in problems) == is_problem
 
     @pytest.mark.parametrize("strict,is_problem", [
                              (True, True),
                              (False, False),
                              ])
-    def test_missing_vice(self, prefs, strict, is_problem):
-        char_file = fixture_dir('linter', 'werewolf', 'Gotta Nada.nwod')
-        character = npc.parser.parse_character(char_file)
-        problems = npc.linters.werewolf.lint(character, strict=strict, prefs=prefs)
+    def test_missing_vice(self, strict, is_problem):
+        problems = lint_werewolf('Gotta Nada.nwod', strict=strict)
         assert ('Missing vice' in problems) == is_problem
 
-def test_tribe_not_recognized(prefs):
-    char_file = fixture_dir('linter', 'werewolf', 'Bad Tribe.nwod')
-    character = npc.parser.parse_character(char_file)
-    problems = npc.linters.werewolf.lint(character, prefs=prefs)
+def test_tribe_not_recognized():
+    problems = lint_werewolf('Bad Tribe.nwod')
     assert "Unrecognized tribe 'Go Pats'" in problems
 
 class TestAuspiceNotPresent:
-    def test_moon_tribe_adds_error(self, prefs):
-        char_file = fixture_dir('linter', 'werewolf', 'No Auspice.nwod')
-        character = npc.parser.parse_character(char_file)
-        problems = npc.linters.werewolf.lint(character, prefs=prefs)
+    def test_moon_tribe_adds_error(self):
+        problems = lint_werewolf('No Auspice.nwod')
         assert "Missing auspice" in problems
 
 class TestAuspicePresent:
-    def test_pure_tribe_adds_error(self, prefs):
-        char_file = fixture_dir('linter', 'werewolf', 'Pure with Auspice.nwod')
-        character = npc.parser.parse_character(char_file)
-        problems = npc.linters.werewolf.lint(character, prefs=prefs)
+    def test_pure_tribe_adds_error(self):
+        problems = lint_werewolf('Pure with Auspice.nwod')
         assert "Auspice present, but werewolf is Pure" in problems
 
-    def test_pure_tribe_adds_error(self, prefs):
-        char_file = fixture_dir('linter', 'werewolf', 'Bad Auspice.nwod')
-        character = npc.parser.parse_character(char_file)
-        problems = npc.linters.werewolf.lint(character, prefs=prefs)
+    def test_pure_tribe_adds_error(self):
+        problems = lint_werewolf('Bad Auspice.nwod')
         assert "Unrecognized auspice 'Crazy Town'" in problems
