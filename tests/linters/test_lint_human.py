011a9e81d868a3c1bf044ec05ea0cc8a83de53d8
Peter Andrews <peter.andrews@modolabs.com>
2018-04-18 13:36:44 -0400

    Expand human vice and virtue tests

--- tests/linters/test_lint_human.py
+++ tests/linters/test_lint_human.py
@@ -6,34 +6,48 @@ import os
 import functools
 from tests.util import fixture_dir
 
-lint_strict = functools.partial(npc.linters.human.lint, strict=True)
-
 def test_requires_path():
     character = npc.Character()
     problems = npc.linters.human.lint(character)
     assert 'Missing path' in problems
 
-class TestStrictHumanLinting:
-    def test_has_virtue(self):
+class TestViceAndVirtue:
+    @pytest.mark.parametrize("strict,is_problem", [
+                             (True, False),
+                             (False, False),
+                             ])
+    def test_has_virtue(self, prefs, strict, is_problem):
         char_file = fixture_dir('linter', 'human', 'Has Virtue.nwod')
         character = npc.parser.parse_character(char_file)
-        problems = lint_strict(character)
-        assert 'Missing virtue' not in problems
+        problems = npc.linters.human.lint(character, strict=strict)
+        assert ('Missing virtue' in problems) == is_problem
 
-    def test_missing_virtue(self):
+    @pytest.mark.parametrize("strict,is_problem", [
+                             (True, True),
+                             (False, False),
+                             ])
+    def test_missing_virtue(self, prefs, strict, is_problem):
         char_file = fixture_dir('linter', 'human', 'Gotta Nada.nwod')
         character = npc.parser.parse_character(char_file)
-        problems = lint_strict(character)
-        assert 'Missing virtue' in problems
+        problems = npc.linters.human.lint(character, strict=strict)
+        assert ('Missing virtue' in problems) == is_problem
 
-    def test_has_vice(self):
+    @pytest.mark.parametrize("strict,is_problem", [
+                             (True, False),
+                             (False, False),
+                             ])
+    def test_has_vice(self, prefs, strict, is_problem):
         char_file = fixture_dir('linter', 'human', 'Has Vice.nwod')
         character = npc.parser.parse_character(char_file)
-        problems = lint_strict(character)
-        assert 'Missing vice' not in problems
+        problems = npc.linters.human.lint(character, strict=strict)
+        assert ('Missing vice' in problems) == is_problem
 
-    def test_missing_vice(self):
+    @pytest.mark.parametrize("strict,is_problem", [
+                             (True, True),
+                             (False, False),
+                             ])
+    def test_missing_vice(self, prefs, strict, is_problem):
         char_file = fixture_dir('linter', 'human', 'Gotta Nada.nwod')
         character = npc.parser.parse_character(char_file)
-        problems = lint_strict(character)
-        assert 'Missing vice' in problems
+        problems = npc.linters.human.lint(character, strict=strict)
+        assert ('Missing vice' in problems) == is_problem
