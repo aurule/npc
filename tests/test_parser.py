4f605e74da250cabc50ad5f9450795f35b2ccf70
Peter Andrews <pmandrews@gmail.com>
2019-06-12 15:53:44 -0400

    Update parser tests

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -2,32 +2,34 @@ import npc
 import pytest
 from tests.util import fixture_dir
 
+from npc.character.tags import UnknownTag
+
 def test_remove_filename_comments():
     parseables = fixture_dir('parsing', 'characters', 'Fetches', 'macho mannersson - faker.nwod')
     characters = list(npc.parser.get_characters(search_paths=[parseables]))
-    assert characters[0].get_first('name') == 'macho mannersson'
+    assert characters[0].tags('name')[0] == 'macho mannersson'
 
 class TestSpecialCharacters:
 
     def test_allow_apostrophes_in_names(self):
         parseables = fixture_dir('parsing', 'characters', 'Fetches', "manny o'mann - super faker.nwod")
         characters = list(npc.parser.get_characters(search_paths=[parseables]))
-        assert characters[0].get_first('name') == "manny o'mann"
+        assert characters[0].tags('name')[0] == "manny o'mann"
 
     def test_allow_periods_in_names(self):
         parseables = fixture_dir('parsing', 'characters', 'Fetches', "Dr. Manny Mann - fakier.nwod")
         characters = list(npc.parser.get_characters(search_paths=[parseables]))
-        assert characters[0].get_first('name') == "Dr. Manny Mann"
+        assert characters[0].tags('name')[0] == "Dr. Manny Mann"
 
     def test_allow_hyphens_in_names(self):
         parseables = fixture_dir('parsing', 'characters', 'Fetches', "Manny Manly-Mann - fakiest.nwod")
         characters = list(npc.parser.get_characters(search_paths=[parseables]))
-        assert characters[0].get_first('name') == "Manny Manly-Mann"
+        assert characters[0].tags('name')[0] == "Manny Manly-Mann"
 
     def test_allow_commas_in_names(self):
         parseables = fixture_dir('parsing', 'characters', 'Fetches', "Manners Mann, Ph.D. - fakierest.nwod")
         characters = list(npc.parser.get_characters(search_paths=[parseables]))
-        assert characters[0].get_first('name') == "Manners Mann, Ph.D."
+        assert characters[0].tags('name')[0] == "Manners Mann, Ph.D."
 
 class TestInclusion:
     """Tests which files are included in the parsed data"""
@@ -37,7 +39,7 @@ class TestInclusion:
         ignore_me = fixture_dir('parsing', 'characters', 'Fetches')
         characters = npc.parser.get_characters(search_paths=[parseables], ignore_paths=[ignore_me])
         for c in characters:
-            assert c.get_first('type') != 'Fetch'
+            assert c.tags('type')[0] != 'Fetch'
 
     def test_ignore_file(self):
         parseables = fixture_dir('parsing', 'characters')
@@ -99,24 +101,24 @@ class TestTags:
     def test_changeling_shortcut(self, character):
         """@changeling should set type, seeming, and kith"""
         c = character('Changeling Tag.nwod')
-        assert c.get_first('type') == 'Changeling'
-        assert c.get_first('seeming') == 'Beast'
-        assert c.get_first('kith') == 'Hunterheart'
+        assert c.tags('type')[0] == 'Changeling'
+        assert c.tags('seeming')[0] == 'Beast'
+        assert c.tags('kith')[0] == 'Hunterheart'
 
     def test_realname(self, character):
         """@realname should overwrite the first name entry"""
         c = character('File Name.nwod')
-        assert c.get_first('name') == 'Real Name'
+        assert c.tags('name')[0] == 'Real Name'
 
     def test_group_rank(self, character):
         """@rank should scope its value to the most recent @group"""
         c = character('Group Rank.nwod')
-        assert c.tags['rank'] == {'Frat': ['Brother']}
+        assert 'Brother' in c.tags('group')['Frat']
 
     def test_bare_rank(self, character):
-        """@rank should not be added without a prior @group"""
+        """when there's a bare rank with no group, it gets added to the top level as an UnknownTag"""
         c = character('Bare Rank.nwod')
-        assert not c.tags['rank']
+        assert type(c.tags['rank']) == UnknownTag
 
 class TestNames:
     """Tests the way character names are grabbed from filenames"""
@@ -131,8 +133,21 @@ class TestNames:
 
     def test_notes(self, character):
         c = character('Standard Dude - dude man.nwod')
-        assert c.get_first('name') == 'Standard Dude'
+        assert c.tags('name')[0] == 'Standard Dude'
 
     def test_doctor(self, character):
         c = character('Dr. Manly Mann.nwod')
-        assert c.get_first('name') == "Dr. Manly Mann"
+        assert c.tags('name')[0] == "Dr. Manly Mann"
+
+class TestHiding:
+    @pytest.fixture
+    def character(self):
+        def make_character(filename):
+            parseables = fixture_dir('parsing', 'tags', filename)
+            characters = list(npc.parser.get_characters(search_paths=[parseables]))
+            return characters[0]
+        return make_character
+
+    def test_hide_marks_right_tag_as_hidden(self, character):
+        c = character('Hidden Dead.nwod')
+        assert c.tags('dead').hidden
