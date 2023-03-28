bf5f1aee4365fd7c76417e2cd30b2b97d8fe6733
Paige Andrews <paige.andrews@modolabs.com>
2023-03-28 17:13:42 -0400

    Add new settings and templates scaffolds

--- src/npc/__init__.py
+++ src/npc/__init__.py
@@ -2,4 +2,6 @@
 Package for reading and manipulating campaign information.
 """
 
-from . import commands, settings, character, cli, __version__
+# from . import commands, settings, character, cli
+
+__version__ = "2.0.0 dev"
