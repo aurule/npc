a6a70ce8bff2acf14d8edfe13a884d0f462d31e0
Paige Andrews <paige.andrews@modolabs.com>
2023-03-30 11:12:20 -0400

    Add new schema mismatch error

--- src/npc/util/errors.py
+++ src/npc/util/errors.py
@@ -31,3 +31,15 @@ class ParseError(Error):
         self.path = path
         self.lineno = lineno
         self.colno = colno
+
+"""Raised when there is a conflict between expected setting value types
+
+Args:
+    strerror (str): Error message describing what happened
+    path (PathLike): Path to the offending file. Usually None
+    lineno (int): Line number of the failure, if known. Defaults to 0.
+    colno (int): Column of the failure, if known. Defaults to 0.
+"""
+class SchemaError(Error):
+    def __init__(self, strerror: str, path = None, lineno: int = 0, colno: int = 0):
+        self.strerror = strerror
