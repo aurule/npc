134ffc5896c05c7ef8b1ceb99e1868af409c8525
Paige Andrews <pmandrews@gmail.com>
2023-03-29 21:07:57 -0400

    Add more docblocks

--- src/npc/util/functions.py
+++ src/npc/util/functions.py
@@ -6,8 +6,7 @@ import yaml
 from . import errors
 
 def parse_yaml(filename):
-    """
-    Parse a YAML file
+    """Parse a YAML file
 
     Args:
         filename (str): Path of the file to load
@@ -24,6 +23,18 @@ def parse_yaml(filename):
             raise errors.ParseError(nicemsg, filename, err.problem_mark.line, err.problem_mark.column)
 
 def index_compare(target_list: list, filter: callable) -> int:
+    """Get the index of the first item in a list for which the filter returns true
+    
+    Uses callable to test every element of target_list. The first matching element has its index returned. If 
+    none match, returns -1.
+    
+    Args:
+        target_list (list): List to search
+        filter (callable): Function to use to test each element. Must accept a single value.
+    
+    Returns:
+        int: The index of the first element which matches the filter function, or -1 if none match
+    """
     for index, value in enumerate(target_list):
         if filter(value):
             return index
