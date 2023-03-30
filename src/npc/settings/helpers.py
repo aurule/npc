9c711adb4a7bd67fbf31379001e184b8c7edb01e
Paige Andrews <paige.andrews@modolabs.com>
2023-03-30 11:12:43 -0400

    Document merge_settings_dicts

--- src/npc/settings/helpers.py
+++ src/npc/settings/helpers.py
@@ -5,7 +5,30 @@ Helper functions unique to the settings module
 from ..util import index_compare
 
 def merge_settings_dicts(new_data: dict, orig: dict) -> dict:
-    dest = dict(orig)
+    """Merge one dictionary into another, and return the result
+
+    This method does not modify the original dict.
+
+    Add the keys in new_data to the orig dict. The logic for this merge is:
+    1. If a key does not exist in orig, add it
+    2. If orig[key] and new_data[key] are dicts, recurse
+    3. If orig[key] and new_data[key] are lists, call merge_tags_lists to handle that logic
+    4. Overwrite orig[key] with new_data[key]
+
+    If orig[key] and new_data[key] have different types, npc.errors.SchemaError is raised.
+
+    Args:
+        new_data (dict): Dict to merge into orig
+        orig (dict): Dict to receive new or updated keys
+
+    Returns:
+        dict: Result of the merge
+
+    Raises:
+        errors: npc.errors.SchemaError when the target key expects a dict or list, but receives something else
+                in new_data
+    """
+    dest: dict = dict(orig)
 
     for key, val in new_data.items():
         if key not in dest:
