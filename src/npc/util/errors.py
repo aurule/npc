edd06091d6c84defa1b9056ab1fe776de4eaf757
Paige Andrews <paige.andrews@modolabs.com>
2023-03-30 11:12:11 -0400

    Fix location of doc strings

--- src/npc/util/errors.py
+++ src/npc/util/errors.py
@@ -2,31 +2,31 @@
 Classes to represent error states
 """
 
+"""Base exception class for NPC"""
 class Error(Exception):
-    """Base exception class for NPC"""
     pass
 
-class FormatError(Error):
-    """
-    Raised when trying to use a malformed file
+"""
+Raised when trying to use a malformed file
 
-    Attributes:
-        strerror (str): Error message describing what happened
-    """
+Attributes:
+    strerror (str): Error message describing what happened
+"""
+class FormatError(Error):
     def __init__(self, strerror):
         self.strerror = strerror
 
-class ParseError(Error):
-    """
-    Raised when a parsing operation fails
+"""
+Raised when a file parsing operation fails
 
-    Attributes:
-        strerror (str): Error message describing what happened
-        path (PathLike): Path to the offending file
-        lineno (int): Line number of the failure, if known. Defaults to 0.
-        colno (int): Column of the failure, if known. Defaults to 0.
-    """
-    def __init__(self, strerror, path, lineno=0, colno=0):
+Attributes:
+    strerror (str): Error message describing what happened
+    path (PathLike): Path to the offending file
+    lineno (int): Line number of the failure, if known. Defaults to 0.
+    colno (int): Column of the failure, if known. Defaults to 0.
+"""
+class ParseError(Error):
+    def __init__(self, strerror: str, path, lineno: int = 0, colno: int = 0):
         self.strerror = strerror
         self.path = path
         self.lineno = lineno
