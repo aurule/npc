678d011b97e027a9e7f3d12bd6527e6cf46d15ea
Paige Andrews <paige.andrews@modolabs.com>
2023-04-04 17:04:06 -0400

    Add campaign fixture for ease of use

--- tests/fixtures/helpers.py
+++ tests/fixtures/helpers.py
@@ -1,6 +1,13 @@
+import pytest
 from pathlib import Path
 from importlib import resources
+
+from npc.campaign import init
 
 def fixture_file(fixture_path: list[str]) -> Path:
     base: Path = resources.files("tests.fixtures")
     return base.joinpath(*fixture_path)
+
+@pytest.fixture
+def tmp_campaign(tmp_path):
+    return (tmp_path, init(tmp_path, name="Test Campaign", system="generic"))
