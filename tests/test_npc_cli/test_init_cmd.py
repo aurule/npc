1bcf08bbfed49db2a0d8aefb46930b655d062e82
Paige Andrews <paige.andrews@modolabs.com>
2023-04-13 16:23:09 -0400

    Put assertions in the with block

--- tests/test_npc_cli/test_init_cmd.py
+++ tests/test_npc_cli/test_init_cmd.py
@@ -9,8 +9,8 @@ def test_defaults_to_current_dir(tmp_path):
     with runner.isolated_filesystem(temp_dir=tmp_path) as td:
         result = runner.invoke(cli, ["init", "--system", "fate"])
 
-    assert result.exit_code == 0
-    assert Path(td).joinpath(".npc").exists()
+        assert result.exit_code == 0
+        assert Path(td).joinpath(".npc").exists()
 
 def test_accepts_target_dir(tmp_path):
     runner = CliRunner()
@@ -18,14 +18,14 @@ def test_accepts_target_dir(tmp_path):
     with runner.isolated_filesystem(temp_dir=tmp_path) as td:
         result = runner.invoke(cli, ["init", "campaign", "--system", "fate"])
 
-    assert result.exit_code == 0
-    assert Path(td).joinpath("campaign", ".npc").exists()
+        assert result.exit_code == 0
+        assert Path(td).joinpath("campaign", ".npc").exists()
 
 def test_lists_dirs_to_create(tmp_path):
     runner = CliRunner()
 
-    with runner.isolated_filesystem(temp_dir=tmp_path) as td:
+    with runner.isolated_filesystem(temp_dir=tmp_path):
         result = runner.invoke(cli, ["init", "campaign", "--system", "fate"])
 
-    assert result.exit_code == 0
-    assert "Session History" in result.output
+        assert result.exit_code == 0
+        assert "Session History" in result.output
