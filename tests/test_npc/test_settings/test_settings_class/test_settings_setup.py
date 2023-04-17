93376059769eed824e4836326872da3d6c7c2064
Paige Andrews <pmandrews@gmail.com>
2023-04-16 21:19:20 -0400

    Remove old planned feature tests

--- tests/test_npc/test_settings/test_settings_class/test_settings_setup.py
+++ tests/test_npc/test_settings/test_settings_class/test_settings_setup.py
@@ -13,12 +13,6 @@ def test_loads_default_systems():
     
     assert "nwod" in settings.get("npc.systems").keys()
 
-@pytest.mark.xfail
-def test_loads_default_types():
-    settings = Settings()
-
-    assert "changeling" in settings.get("npc.systems.nwod.types").keys()
-
 def test_loads_personal_settings(tmp_path):
     settings = Settings(personal_dir = fixture_file("personal", "simple"))
 
@@ -28,9 +22,3 @@ def test_loads_personal_systems():
     settings = Settings(personal_dir = fixture_file("personal", "large"))
 
     assert "custom" in settings.get("npc.systems").keys()
-
-@pytest.mark.xfail
-def test_loads_personal_types():
-    settings = Settings(personal_dir = fixture_file("personal", "chonk"))
-    
-    assert "pet" in settings.get("npc.systems.generic.types").keys()
