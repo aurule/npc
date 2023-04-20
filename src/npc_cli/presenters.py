f4410632903ec7afff21f58413216215904b5c8e
Paige Andrews <paige.andrews@modolabs.com>
2023-04-20 16:12:20 -0400

    Add tabularize cli helper

--- src/npc_cli/presenters.py
+++ src/npc_cli/presenters.py
@@ -31,3 +31,45 @@ def campaign_info(campaign: Campaign) -> str:
         f"{'Sessions:':>12} {campaign.latest_session_index}",
     ]
     return "\n".join(infos)
+
+def tabularize(rows: list[list], headers: list[str], title: str = None) -> str:
+    """Generate a multimarkdown-style table
+
+    Creates a table with the given data, headers, and optional title. Tables look like:
+
+                                [Character Types for New World of Darkness]
+    | Name       | Key        | Description                                                           |
+    |------------|------------|-----------------------------------------------------------------------|
+    | Werewolf   | werewolf   | Shapechanging warriors who guard the spirit world                     |
+    | Spirit     | spirit     | A disembodied representation of a thing, place, or concept            |
+    | Vampire    | vampire    | A cursed denizen of the night who feeds on blood.                     |
+    | Mage       | mage       | An awakened human with access to world-shaping magic.                 |
+    | Changeling | changeling | A human who was captured by the fae and returned to the mundane world |
+
+    Args:
+        rows (list[list]): List of row data. Each row is a list whose contents are the values of each column.
+        headers (list[str]): List of header strings.
+        title (str): Title to add to the table (default: `None`)
+
+    Returns:
+        str: Formatted table of data
+    """
+    rows.insert(0, headers)
+    colwidths: list = []
+    for index in range(len(headers)):
+        colwidths.append(max([len(row[index]) for row in rows]))
+
+    lines: list[str] = [
+        "| " + " | ".join([f"{rows[0][colnum]:<{width}}" for colnum, width in enumerate(colwidths)]) + " |",
+        "|" + "|".join(["-"*(width+2) for width in colwidths]) + "|"
+    ]
+
+    if title:
+        title = f"[{title}]"
+        title_offset: float = (len(title) + len(lines[0]))/2
+        lines.insert(0, f"{title:>{title_offset:.0f}}")
+
+    for row in rows[1::]:
+        lines.append("| " + " | ".join([f"{row[colnum]:<{width}}" for colnum, width in enumerate(colwidths)]) + " |")
+
+    return "\n".join(lines)
---
+++ tests/test_npc_cli/test_presenters/test_tabularize.py
@@ -0,0 +1,16 @@
+from npc_cli.presenters import tabularize
+
+def test_inserts_title():
+    table = tabularize([[]], [], title = "Test Title")
+
+    assert "[Test Title]" in table
+
+def test_includes_headers():
+    table = tabularize([["a", "b"]], ["Test", "Header"])
+
+    assert "Header" in table
+
+def test_shows_data():
+    table = tabularize([["first", "second"]], ["Test", "Header"])
+
+    assert "first" in table
