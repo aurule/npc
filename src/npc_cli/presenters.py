f9a7045c85a2533e578171b67ce77f7413f8194b
Paige Andrews <paige.andrews@modolabs.com>
2023-04-20 16:12:07 -0400

    Document existing cli presenters

--- src/npc_cli/presenters.py
+++ src/npc_cli/presenters.py
@@ -1,9 +1,27 @@
 from npc.campaign import Campaign
 
 def directory_list(dirs: list) -> str:
+    """Format a list of directory names
+
+    Args:
+        dirs (list): List of Paths to one or more directories
+
+    Returns:
+        str: Description of the given Paths
+    """
     return "\n".join([f"  {dirname}/" for dirname in dirs])
 
 def campaign_info(campaign: Campaign) -> str:
+    """Show information about a campaign
+
+    Shows some brief info and stats
+
+    Args:
+        campaign (Campaign): Campaign to describe
+
+    Returns:
+        str: Info about the campaign
+    """
     infos = [
         f"{'---Campaign Info---':>22}",
         f"{'Name:':>12} {campaign.name}",
