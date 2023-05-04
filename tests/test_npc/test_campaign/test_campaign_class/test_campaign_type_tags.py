232bde03c398de85a8f543d1f2230169287a95dc
Paige Andrews <paige.andrews@modolabs.com>
2023-05-04 10:30:58 -0400

    Fix campaign type tags test

--- tests/test_npc/test_campaign/test_campaign_class/test_campaign_type_tags.py
+++ tests/test_npc/test_campaign/test_campaign_class/test_campaign_type_tags.py
@@ -17,8 +17,15 @@ def test_includes_system_tags(tmp_campaign):
     assert "concept" in tags
 
 def test_includes_campaign_tags(tmp_campaign):
+    tmp_campaign.patch_campaign_settings({
+        "tags": {
+            "test": {
+                "desc": "Marks as test"
+            }
+        }
+    })
     tmp_campaign.patch_campaign_settings({"system": "nwod"})
 
     tags = tmp_campaign.type_tags("changeling")
 
-    assert "motley" in tags
+    assert "test" in tags
