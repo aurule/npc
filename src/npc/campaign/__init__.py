b290a87a6f8d3cfc5068ada3047ca4e63503bb80
Paige Andrews <pmandrews@gmail.com>
2023-05-14 17:59:51 -0400

    Make pathfinder a class

--- src/npc/campaign/__init__.py
+++ src/npc/campaign/__init__.py
@@ -1,2 +1,3 @@
 from .ops import *
 from .campaign_class import Campaign
+from .pathfinder_class import Pathfinder
---
+++ src/npc/campaign/pathfinder_class.py
@@ -0,0 +1,37 @@
+from sqlalchemy import select
+from pathlib import Path
+
+from .campaign_class import Campaign
+from ..characters import Character, Tag
+from ..db import DB
+
+class Pathfinder:
+    def __init__(self, campaign: Campaign):
+        self.campaign = campaign
+        self.path_components = campaign.settings.get("campaign.characters.subpath_components")
+        self.base_path = campaign.characters_dir
+
+    def build_character_path(self, character: Character, *, exists: bool = True) -> Path:
+        character_path: Path = self.base_path
+        for component in self.path_components:
+            match component.get("selector"):
+                case "first_value":
+                    filters: dict = {}
+
+                    if exists:
+                        existing_dirs = [child for child in character_path.iterdir() if child.is_dir()]
+                        filters.value = existing_dirs
+
+                    db = DB()
+                    with db.session() as session:
+                        stmt = select(Tag).where(Tag.name.in_(component.get("tags", []))).filter_by(**filters).order_by(Tag.id)
+                        result_tag = session.execute(stmt).first()
+
+                    if result_tag:
+                        print(character_path.joinpath(result_tag[0].value))
+                        character_path = character_path.joinpath(result_tag[0].value)
+                case _:
+                    pass
+                    # unsupported selector
+
+        return character_path
