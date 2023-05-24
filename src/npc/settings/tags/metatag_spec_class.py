91b42f54390f593d28dc34f4e062edf602c478d8
Paige Andrews <pmandrews@gmail.com>
2023-05-23 20:29:26 -0400

    Add required names to metatag spec

--- src/npc/settings/tags/metatag_spec_class.py
+++ src/npc/settings/tags/metatag_spec_class.py
@@ -1,4 +1,6 @@
 class MetatagSpec():
+    """Represents a metatag definition"""
+
     def __init__(self, name: str, tag_def: dict):
         self.name: str              = name
         self.definition: dict       = tag_def
@@ -8,3 +10,14 @@ class MetatagSpec():
         self.match: list[str]       = tag_def.get("match", [])
         self.separator: str         = tag_def.get("separator", " ")
         self.greedy: bool           = tag_def.get("greedy", False)
+
+    @property
+    def required_tag_names(self) -> list[str]:
+        """Get a list of required tags
+
+        These tag names must be present in a character for this spec to be used
+
+        Returns:
+            list[str]: List of tag names required for this spec
+        """
+        return list(self.static) + self.match
