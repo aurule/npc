536b8c694fa09d593fb8ccb409e59517420afa33
Paige Andrews <paige.andrews@modolabs.com>
2023-05-24 14:36:09 -0400

    Add envvar helper

--- src/npc/util/__init__.py
+++ src/npc/util/__init__.py
@@ -3,7 +3,7 @@ Helper functions shared between the other modules
 """
 
 from .functions import *
-from . import errors
+from . import errors, env
 from .data_store import DataStore
 from .singleton import Singleton
 from .win_sanitize import *
