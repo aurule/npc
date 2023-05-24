d3ab7a8385b4166ad6a49b010b94de6a03d0c098
Paige Andrews <pmandrews@gmail.com>
2023-05-23 20:17:46 -0400

    Improve typing of metatag spec

--- src/npc/settings/tags/metatag_spec_class.py
+++ src/npc/settings/tags/metatag_spec_class.py
@@ -1,10 +1,10 @@
 class MetatagSpec():
     def __init__(self, name: str, tag_def: dict):
-        self.name: str          = name
-        self.definition: dict   = tag_def
-        self.desc: str          = tag_def.get("desc", "")
-        self.doc: str           = tag_def.get("doc", "")
-        self.static: dict       = tag_def.get("static", {})
-        self.match: list        = tag_def.get("match", [])
-        self.separator          = tag_def.get("separator", " ")
-        self.greedy             = tag_def.get("greedy", False)
+        self.name: str              = name
+        self.definition: dict       = tag_def
+        self.desc: str              = tag_def.get("desc", "")
+        self.doc: str               = tag_def.get("doc", "")
+        self.static: dict[str, str] = tag_def.get("static", {})
+        self.match: list[str]       = tag_def.get("match", [])
+        self.separator: str         = tag_def.get("separator", " ")
+        self.greedy: bool           = tag_def.get("greedy", False)
