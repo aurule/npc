0980ab6dbecd4100b763fb3878107e62f204c34f
Paige Andrews <paige.andrews@modolabs.com>
2023-06-09 17:08:20 -0400

    WIP character tag linting

--- src/npc/linters/character_linter.py
+++ src/npc/linters/character_linter.py
@@ -1,6 +1,7 @@
 from npc.campaign import Campaign
-from npc.characters import Character
+from npc.characters import Character, CharacterReader, CharacterFactory
 from npc.validation import CharacterValidator, TagValidator
+from .tag_bucket import TagBucket
 
 class CharacterLinter:
     def __init__(self, character: Character, campaign: Campaign):
@@ -9,24 +10,57 @@ class CharacterLinter:
         self.errors = []
 
     def lint(self) -> list:
-        self.errors = []
-        # character is not expected to be live
-        # get error messages from the character validator
-        # gather tag specs from self.campaign
-        #   for each required spec
-        #       run a TagValidator against the tags in that set
-        #       extend our errors with those from the tag validator
-        #       if the spec allows subtags, iterate those tag records
-        #           run a subtag linter against the each tag record's subtags as needed
-        #       note handled tag IDs
-        # iterate character's unhandled tags
-        #   run a validator on each set
-        #   if the spec allows subtags, iterate tag records
-        #       run a subtag linter on each record
-        # iterate deprecated tag specs
-        #   make sure we don't have any of those
-        return self.errors
+        character_validator = CharacterValidator(self.campaign)
+        self.errors = character_validator.validate(self.character)
 
-    @property
-    def ok(self) -> bool:
-        return not self.errors
+        tag_bucket = TagBucket(self.character)
+
+        reader = CharacterReader(self.character.file_path)
+        factory = CharacterFactory(self.campaign)
+        tag_context_stack = [tag_bucket]
+        for rawtag in reader.tags():
+            factory.apply_raw_tag(rawtag, tag_bucket, tag_context_stack)
+
+
+        self.check_tags(tag_bucket, self.campaign.tags.values())
+
+    def check_tags(self, bucket: TagBucket, available_specs: list):
+        handled_specs = []
+
+        required_specs = (spec for spec in available_specs if (spec.required or spec.min))
+        for spec in required_specs:
+            print(f"{spec.name} is required")
+            self.validate_spec(spec, bucket.tags[spec.name])
+            self.handled_specs.append(spec.name)
+
+        # handle deprecated tags
+        # handled_specs.extend(deprecations)
+
+        remaining_specs = (spec for spec in available_specs if spec.name not in handled_specs and spec.name in bucket.tags.keys())
+        for spec in remaining_specs:
+            print(f"{spec.name} was found")
+            self.validate_spec(spec, bucket.tags[spec.name])
+
+        # handle tags with no spec
+
+    def validate_spec(self, spec, tags: list):
+        """Validate a list of tags against the given spec, then check their subtags
+
+        For most tags, this simply uses a TagValidator on the list of supplied tags. For tags whose definition
+        allows subtags, a new set of allowed subtag specs is created. For every tag in our list, a new list of
+        subtags is created and checked using check_tags() to ensure requirements, deprecations, etc. are
+        handled properly.
+
+        Args:
+            spec (TagSpec): The spec to check the tags against
+            tags (list): The tags to be checked
+        """
+        validator = TagValidator(spec)
+        self.errors.extend(validator.validate(tags))
+        if spec.subtags:
+            subtag_specs = [self.campaign.get_tag(subtag_name).in_context(spec.name) for subtag_name in spec.subtags]
+            subtag_bucket = TagBucket()
+            for tag in tags:
+                for subtag in tag.subtags:
+                    subtag_bucket.add_tag(subtag)
+                self.check_tags(subtag_bucket, subtag_specs)
---
+++ tests/test_npc/test_linters/test_character_linter/test_check_tags.py
@@ -0,0 +1,23 @@
+from tests.fixtures import tmp_campaign
+
+from npc.settings.tags import TagSpec
+from npc.characters import Character, Tag
+from npc.campaign import Campaign
+from npc.settings.tags import make_tags
+
+from npc.linters import CharacterLinter
+
+def test_checks_required_tags(tmp_campaign):
+    pass
+
+def test_checks_deprecated_tags(tmp_campaign):
+    pass
+
+def test_checks_remaining_tags(tmp_campaign):
+    pass
+
+def test_checks_required_once(tmp_campaign):
+    pass
+
+def test_checks_deprecated_once(tmp_campaign):
+    pass
