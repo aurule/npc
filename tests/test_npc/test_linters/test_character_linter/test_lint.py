1a0914dc60fc8375539d94104e31fcd5c0985c65
Paige Andrews <pmandrews@gmail.com>
2023-06-10 18:05:41 -0400

    WIP lint test

---
+++ tests/test_npc/test_linters/test_character_linter/test_lint.py
@@ -0,0 +1,13 @@
+from tests.fixtures import tmp_campaign
+
+from npc.characters import Tag, Character
+from npc.linters.tag_bucket import TagBucket
+from npc.settings.tags import TagSpec
+
+from npc.linters import CharacterLinter
+
+def test_checks_char_attributes(tmp_campaign):
+    pass
+
+def test_checks_tags(tmp_campaign):
+    pass
