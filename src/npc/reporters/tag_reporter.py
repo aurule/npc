275c910870d4939e9867784e681cf1a1d395927f
Paige Andrews <pmandrews@gmail.com>
2023-06-13 23:56:53 -0400

    Flesh out tag reporter

--- src/npc/reporters/tag_reporter.py
+++ src/npc/reporters/tag_reporter.py
@@ -1,14 +1,24 @@
-from npc.db import DB, tag_repository
+from npc.db import DB, tag_repository, character_repository
 from npc.characters import Character, Tag
 
-def value_counts_report(tag_name, db=None):
+def value_counts_report(tag_name: str, db: DB = None):
+    """Get report data that counts occurrences of all unique values for the named tag
+
+    This either runs a query on Tag, or a query on Character for tags which are technically mapped to an
+    attribute.
+
+    Args:
+        tag_name (str): Name of the tag to analyze
+        db (DB): Database to use for querying. Defaults to the singleton. (default: `None`)
+
+    Returns:
+        list[Row]: List of tuple-like Row objects for the results
+    """
     if not db:
         db = DB()
 
     if tag_name in Character.MAPPED_TAGS:
-        pass
-        # need to look up the character attribute to use for the named tag
-        # query needs to look at Character.attribute, but is otherwise very similar
+        query = character_repository.attr_counts(tag_name)
     else:
         query = tag_repository.value_counts(tag_name)
 
