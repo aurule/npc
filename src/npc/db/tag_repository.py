89f9d6a3420101a3d43ecb48ffc6bafd9a8626a6
Paige Andrews <pmandrews@gmail.com>
2023-06-13 23:56:48 -0400

    Document tag repository

--- src/npc/db/tag_repository.py
+++ src/npc/db/tag_repository.py
@@ -1,7 +1,23 @@
+"""Repository of tag-related queries
+
+These queries are limited to ones that do not rely on anything from the Character class. Since tags usually
+belong to a character, most queries end up in the character repository instead.
+"""
+
 from sqlalchemy import select, Select, func, desc
 from npc.characters import Tag
 
 def value_counts(name: str) -> Select:
+    """Create a db query to get a count of all values for a tag
+
+    Builds a query that counts the appearances of each unique value for a tag
+
+    Args:
+        name (str): Name of the tag to check
+
+    Returns:
+        Select: Select object for the tag query
+    """
     return select(Tag.value, func.count(1).label("value_count")) \
         .where(Tag.name == name) \
         .group_by(Tag.value) \
