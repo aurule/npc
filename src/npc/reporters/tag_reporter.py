ec193f1cfb821f1352de4fcfb37034b3bd67d826
Paige Andrews <paige.andrews@modolabs.com>
2023-06-15 14:21:35 -0400

    Force db to be explicitly assigned

--- src/npc/reporters/tag_reporter.py
+++ src/npc/reporters/tag_reporter.py
@@ -1,7 +1,7 @@
 from npc.db import DB, tag_repository, character_repository
 from npc.characters import Character, Tag
 
-def value_counts_report(tag_name: str, db: DB = None):
+def value_counts_report(tag_name: str, *, db: DB = None):
     """Get report data that counts occurrences of all unique values for the named tag
 
     This either runs a query on Tag, or a query on Character for tags which are technically mapped to an
