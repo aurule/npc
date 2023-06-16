e45019c801307b70b6b9b5513cbd7c8b4f072646
Paige Andrews <paige.andrews@modolabs.com>
2023-06-16 13:57:16 -0400

    Add fancy error output using rich

--- src/npc_cli/__init__.py
+++ src/npc_cli/__init__.py
@@ -2,4 +2,11 @@
 Package for the npc command-line program
 """
 
+try:
+    import click, sqlalchemy
+    from rich.traceback import install
+    install(suppress=[click, sqlalchemy])
+except ImportError:
+    pass
+
 from .commands.main_group import cli
