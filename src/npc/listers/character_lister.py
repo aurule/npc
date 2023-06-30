04b7dc5c11cbfaa3c4aa7b0d286f762092e5b1f8
Paige Andrews <paige.andrews@modolabs.com>
2023-06-30 17:05:03 -0400

    WIP update lister plan

--- src/npc/listers/character_lister.py
+++ src/npc/listers/character_lister.py
@@ -3,6 +3,7 @@ from jinja2 import Environment
 from npc.characters import Character
 from npc.campaign import Campaign, CharacterCollection
 from npc.templates import CharacterFallbackLoader
+from npc.db.query_builders import CharacterListerQueryBuilder
 
 class CharacterLister:
     SUPPORTED_OUTPUT_TYPES = {"html", "markdown"}
@@ -16,39 +17,27 @@ class CharacterLister:
         self.campaign = collection.campaign
         self.lang = lang
 
-        # settings are in campaign.settings.get("campaign.characters.listing")
-
-        # need to group characters
-        #   basically a fancy sort-by
-        #   support tag value
-        #   support meta and non-tag values:
-        #       * realname (first letter group)
-        #       * last_name (first letter group)
-        #       * type
-
-        # need to sort characters:
-        #   sorting is applied after group sort
-
-        # need to get the correct extension based on language
-
         jenv = Environment(
             loader = CharacterFallbackLoader(self.campaign),
             auto_reload = False,
             autoescape = False,
         )
 
-        # apply group_by grouping and sorting to campaign.characters
-        # add sort_by to that query
-        # make a jenv
-        # group_levels = settings.group_by
-        # character_header_level = settings.base_header_level + len(group_levels)
-        # current_group_values = {v: None for v in group_levels}
-        # for character in result set:
-        #   for each grouped value in character:
-        #       if current_group_values[value] is different than ours
-        #           update that value and the value of all groupings after it in group_levels
-        #           emit a group_heading template with the new value
-        #               header level is settings.base_header_level + index of group name in group_levels
+        # get characters!
+        # for entity in settings.group_by:
+        #   builder.group_by(entity)
+        # for entity in settings.sort_by:
+        #   builder.sort_by(entity)
+        #
+        # character_header_level = builder.next_group_index
+        # current_group_values = [None] * builder.next_group_index
+        # for row in results:
+        #   for each group col:
+        #       if current value [index] doesn't match
+        #           update that value
+        #           reset values after it
+        #           ask jenv to get the header template
+        #           emit a group_heading template with the new value at the index header level
         #   ask jenv to get the template <type>.<ext>
         #   make a CharacterView
         #   pass the view through the template
