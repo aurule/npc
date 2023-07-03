43273749f83bdf8b9483d23c3abe1ba4823be474
Paige Andrews <paige.andrews@modolabs.com>
2023-07-03 15:47:10 -0400

    Move reporter commands to their own file

--- src/npc_cli/commands/__init__.py
+++ src/npc_cli/commands/__init__.py
@@ -2,3 +2,4 @@ from .main_group import cli
 from .campaign_group import *
 from .describe_group import *
 from .character_group import *
+from .reports_group import *
--- src/npc_cli/commands/character_group.py
+++ src/npc_cli/commands/character_group.py
@@ -2,9 +2,8 @@ import click
 from click import echo, BadParameter
 
 from npc import characters, linters
-from npc.reporters import tag_reporter
 from npc.util import edit_files
-from npc_cli.presenters import type_list, tabularize
+from npc_cli.presenters import type_list
 from npc_cli.helpers import cwd_campaign, write_new_character
 from npc_cli.errors import CampaignNotFoundException, BadCharacterTypeException
 
@@ -105,51 +104,3 @@ def lint(settings, edit):
 
     if edit and error_characters:
         edit_files(error_characters, settings = settings)
-
-
-##########################
-# Config reports group
-##########################
-
-@cli.group()
-def report():
-    """Show stats about the characters in this campaign"""
-
-
-#######################
-# Show tag stastistics
-#######################
-
-@report.command()
-@click.option("-t", "--tag", "tag_name",
-    help="Tag to analyze")
-@click.option("-c", "--context",
-    help="Parent context if tag is a subtag. Use '*' to disregard parent.")
-@pass_settings
-def values(settings, tag_name, context):
-    """Show a how many times each unique value appears for the given tag
-
-    This command only works within an existing campaign
-
-    The '--context' option is only needed if '--tag' is a subtag like role.
-    """
-    campaign = cwd_campaign(settings)
-    if campaign is None:
-        raise CampaignNotFoundException
-
-    campaign.characters.refresh()
-
-    spec = campaign.get_tag(tag_name)
-    if spec.needs_context and context != "*":
-        valid_contexts = list(spec.contexts.keys()) + ["*"]
-        if (not context) or (context not in valid_contexts):
-            raise click.BadParameter(f"{tag_name} tag requires context to be one of {valid_contexts}", param_hint=['-c', '--context'])
-        data = tag_reporter.subtag_value_counts_report(tag_name, context)
-        prefix: str = f"@{tag_name} ({context})"
-    else:
-        data = tag_reporter.value_counts_report(tag_name)
-        prefix: str = f"@{tag_name} tag"
-
-    title: str = f"{prefix} values report"
-    headers = ("Value", "Count")
-    echo(tabularize(data, headers=headers, title=title))
