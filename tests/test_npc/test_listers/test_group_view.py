aa29a73384d61923fce9ac1071061f55929ae9cc
Paige Andrews <pmandrews@gmail.com>
2023-07-16 13:55:59 -0400

    Test default group view string

--- tests/test_npc/test_listers/test_group_view.py
+++ tests/test_npc/test_listers/test_group_view.py
@@ -9,3 +9,8 @@ def test_uses_grouping():
     view = GroupView("test view", "blerb")
 
     assert view.grouping == "blerb"
+
+def test_default_str():
+    view = GroupView("test view", "blerb")
+
+    assert str(view) == "test view"
