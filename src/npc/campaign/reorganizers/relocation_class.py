acd6596316f8e66499fae03e612867c8b4ab8b31
Paige Andrews <paige.andrews@modolabs.com>
2023-08-07 10:31:06 -0400

    Prevent comparing relocation objects against other types

--- src/npc/campaign/reorganizers/relocation_class.py
+++ src/npc/campaign/reorganizers/relocation_class.py
@@ -16,8 +16,34 @@ class Relocation:
     ideal_path: Path
 
     def __lt__(self, other) -> bool:
+        """Compare against another relocation
+
+        This method is made to be used for sorting lists. A Relocation is considered less than another when
+        its current path matches the other's ideal path. This sorts Relocations such that when applied in
+        order, conflicting files will be moved out of the way before their path is taken.
+
+        Args:
+            other (Relocation): The other relocation to compare against
+
+        Returns:
+            bool: True if our current path matches other's ideal path, False if not
+
+        Raises:
+            NotImplementedError: Relocations cannot be compared against any other type
+        """
+        if not isinstance(other, self.__class__):
+            raise NotImplementedError
+
         return self.current_path == other.ideal_path
 
     @property
     def satisfied(self) -> bool:
+        """Get whether this relocation has been done
+
+        A relocation with matching current and ideal paths does not need to be used, as the file is already at
+        its ideal location.
+
+        Returns:
+            bool: True if current path matches ideal path, false if not
+        """
         return self.current_path == self.ideal_path
--- tests/test_npc/test_campaign/test_reorganizers/test_relocation_class/test_relocation_lt.py
+++ tests/test_npc/test_campaign/test_reorganizers/test_relocation_class/test_relocation_lt.py
@@ -1,3 +1,5 @@
+import pytest
+
 from npc.campaign.reorganizers.relocation_class import Relocation
 
 def test_true_when_currpath_eq_other_ideal():
@@ -11,3 +13,10 @@ def test_false_when_currpath_neq_other_ideal():
     reloc2 = Relocation(id = 2, current_path = "/test/second.txt", ideal_path = "/test/whynot.txt")
 
     assert not reloc2 < reloc1
+
+def test_throws_with_unexpected_object():
+    reloc = Relocation(id = 1, current_path = "/test/first.txt", ideal_path = "/test/first.txt")
+    other = 5
+
+    with pytest.raises(NotImplementedError):
+        reloc < other
