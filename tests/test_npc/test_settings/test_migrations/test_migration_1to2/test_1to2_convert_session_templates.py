76c595592cfa5d40d2e814ea2334217c692e66bf
Paige Andrews <paige.andrews@modolabs.com>
2023-09-15 13:40:59 -0400

    Check for session templates in the right dir

--- tests/test_npc/test_settings/test_migrations/test_migration_1to2/test_1to2_convert_session_templates.py
+++ tests/test_npc/test_settings/test_migrations/test_migration_1to2/test_1to2_convert_session_templates.py
@@ -61,7 +61,8 @@ class TestConvertsSession:
         assert new_data.get("campaign.session.filename_pattern") == "session ((NN)).md"
 
     def test_saves_contents(self, tmp_campaign):
-        old_template = tmp_campaign.settings_dir / "session NN.md"
+        tmp_campaign.settings_dir.joinpath("legacy").mkdir()
+        old_template = tmp_campaign.settings_dir / "legacy" / "session NN.md"
         with old_template.open('w') as f:
             f.write("test things")
         migration = Migration1to2(tmp_campaign.settings)
@@ -83,7 +84,8 @@ class TestConvertsPlot:
         assert not new_data.get("campaign.plot.file_contents")
 
     def test_no_key_yes_file_skips(self, tmp_campaign):
-        old_template = tmp_campaign.settings_dir / "plot NN.md"
+        tmp_campaign.settings_dir.joinpath("legacy").mkdir()
+        old_template = tmp_campaign.settings_dir / "legacy" / "plot NN.md"
         old_template.touch()
         migration = Migration1to2(tmp_campaign.settings)
         old_data = DataStore()
@@ -104,7 +106,8 @@ class TestConvertsPlot:
         assert not new_data.get("campaign.plot.file_contents")
 
     def test_sets_file_name(self, tmp_campaign):
-        old_template = tmp_campaign.settings_dir / "plot NN.md"
+        tmp_campaign.settings_dir.joinpath("legacy").mkdir()
+        old_template = tmp_campaign.settings_dir / "legacy" / "plot NN.md"
         with old_template.open('w') as f:
             f.write("test things")
         migration = Migration1to2(tmp_campaign.settings)
@@ -116,7 +119,8 @@ class TestConvertsPlot:
         assert new_data.get("campaign.plot.filename_pattern")
 
     def test_subs_num_placeholder(self, tmp_campaign):
-        old_template = tmp_campaign.settings_dir / "plot NN.md"
+        tmp_campaign.settings_dir.joinpath("legacy").mkdir()
+        old_template = tmp_campaign.settings_dir / "legacy" / "plot NN.md"
         with old_template.open('w') as f:
             f.write("test things")
         migration = Migration1to2(tmp_campaign.settings)
@@ -128,7 +132,8 @@ class TestConvertsPlot:
         assert new_data.get("campaign.plot.filename_pattern") == "plot ((NN)).md"
 
     def test_saves_contents(self, tmp_campaign):
-        old_template = tmp_campaign.settings_dir / "plot NN.md"
+        tmp_campaign.settings_dir.joinpath("legacy").mkdir()
+        old_template = tmp_campaign.settings_dir / "legacy" / "plot NN.md"
         with old_template.open('w') as f:
             f.write("test things")
         migration = Migration1to2(tmp_campaign.settings)
