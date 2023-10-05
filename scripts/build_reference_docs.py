005854421fe7c7e32d75130666e27d829010f0d8
Paige Andrews <paige.andrews@modolabs.com>
2023-10-05 15:30:55 -0400

    Add system build and tables to docs builder script

--- scripts/build_reference_docs.py
+++ scripts/build_reference_docs.py
@@ -15,8 +15,10 @@ jenv = Environment(
     auto_reload = False,
     autoescape = False,
 )
-tag_template = jenv.get_template("tag.rst.j2")
 
+# Build Tag References
+
+tag_template = jenv.get_template("tag.rst.j2")
 for tag in ref_system.tags.values():
     if tag.needs_context:
         continue
@@ -34,4 +36,36 @@ for tag in ref_system.tags.values():
     with outfile.open("w", newline="\n") as f:
         f.write(data)
 
-# build systems references
+tag_table_template = jenv.get_template("tag_table.rst.j2")
+tag_table_data = tag_table_template.render({
+    "tags": sorted(ref_system.tags.values(), key=lambda s: s.name)
+    })
+tag_table_file = Path(f"docs/reference/tags/components/tag_table.rst")
+with tag_table_file.open("w", newline="\n") as f:
+    f.write(tag_table_data)
+
+# Build Game System References
+
+system_template = jenv.get_template("system.rst.j2")
+systems_summary = []
+for system_key in sorted(settings.get_system_keys()):
+    system = settings.get_system(system_key)
+    systems_summary.append(system)
+    data = system_template.render({
+        "header_characters": header_characters,
+        "header_level": 0,
+        "system": system,
+        "len": len,
+    })
+
+    outfile = Path(f"docs/reference/systems/{system_key}.rst")
+    with outfile.open("w", newline="\n") as f:
+        f.write(data)
+
+system_table_template = jenv.get_template("system_table.rst.j2")
+system_table_data = system_table_template.render({
+    "systems": systems_summary
+    })
+system_table_file = Path(f"docs/reference/systems/components/system_table.rst")
+with system_table_file.open("w", newline="\n") as f:
+    f.write(system_table_data)
