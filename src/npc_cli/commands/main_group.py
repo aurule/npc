80731d456739968e588a363b90db1887531fb346
Paige Andrews <paige.andrews@modolabs.com>
2023-10-17 16:03:30 -0400

    Check that our version is not older than the user's settings version

--- src/npc_cli/commands/main_group.py
+++ src/npc_cli/commands/main_group.py
@@ -1,7 +1,10 @@
 import click
 from os import get_terminal_size
+from packaging.version import Version
 
+from npc import __version__ as npc_version
 from npc.settings import Settings, app_settings
+from npc.settings.migrations import SettingsMigrator
 
 arg_settings: Settings = app_settings()
 
@@ -22,3 +25,9 @@ def cli(ctx):
     ctx.max_content_width = term_width
 
     ctx.show_default = True
+
+    # check that we aren't outdated
+    our_version = Version(npc_version)
+    settings_version = Version(arg_settings.versions.get("user", npc_version))
+    if our_version < settings_version:
+        click.echo(f"WARNING: Installed version of NPC ({our_version}) is older than the one which last updated your user settings ({settings_version}). NPC may behave incorrectly. Please upgrade to the latest release as soon as possible.")
