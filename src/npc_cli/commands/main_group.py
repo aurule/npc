e89eb28bbb273233dd12431152853fa3d1ef4757
Paige Andrews <paige.andrews@modolabs.com>
2023-10-17 16:03:55 -0400

    Migrate user settings if needed

--- src/npc_cli/commands/main_group.py
+++ src/npc_cli/commands/main_group.py
@@ -31,3 +31,24 @@ def cli(ctx):
     settings_version = Version(arg_settings.versions.get("user", npc_version))
     if our_version < settings_version:
         click.echo(f"WARNING: Installed version of NPC ({our_version}) is older than the one which last updated your user settings ({settings_version}). NPC may behave incorrectly. Please upgrade to the latest release as soon as possible.")
+
+    # migrate user settings
+    migrator = SettingsMigrator(arg_settings)
+    if migrator.can_migrate("user"):
+        action = click.prompt(
+            f"Your user settings are out of date and need to be migrated. Do you want to migrate now, open the files for manual inspection, or quit NPC",
+            default="migrate",
+            type=click.Choice(["migrate", "open", "quit"])
+        )
+        match action:
+            case "migrate":
+                click.echo("Migrating...")
+                messages = migrator.migrate("user")
+                for m in messages:
+                    click.echo(m.message)
+                click.echo("Done migrating!\n")
+            case "open":
+                click.launch(str(arg_settings.personal_dir / "settings.yaml"), locate=True)
+                ctx.exit(1)
+            case _:
+                ctx.exit(1)
