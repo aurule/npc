82aefb152abd404e4277546ebf96651f27ffc033
Paige Andrews <pmandrews@gmail.com>
2023-12-20 20:29:36 -0500

    Add script to update the stored version

---
+++ scripts/update_version.py
@@ -0,0 +1,21 @@
+import click
+import re
+from pathlib import Path
+
+@click.command
+@click.option(
+    "-v", "--version",
+    "version",
+    required=True,
+    type=str,
+    )
+def update_version(version):
+    version_file = Path("src/npc/__init__.py")
+    old_contents = version_file.read_text()
+
+    new_contents = re.sub(r"version__(\s+)=(\s+)\"(.*)\"", f"version__ = \"{version}\"", old_contents)
+
+    version_file.write_text(new_contents)
+
+if __name__ == '__main__':
+    update_version()
