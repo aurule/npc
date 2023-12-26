b810c3c92da1dd013d25a267721fad900f5db6cf
Paige Andrews <pmandrews@gmail.com>
2023-12-26 17:59:03 -0500

    Update docs version alongside package version

--- scripts/update_version.py
+++ scripts/update_version.py
@@ -12,10 +12,13 @@ from pathlib import Path
 def update_version(version):
     version_file = Path("src/npc/__init__.py")
     old_contents = version_file.read_text()
-
-    new_contents = re.sub(r"version__(\s+)=(\s+)\"(.*)\"", f"version__ = \"{version}\"", old_contents)
-
+    new_contents = re.sub(r"version__\s+=\s+\"(.*)\"", f"version__ = \"{version}\"", old_contents)
     version_file.write_text(new_contents)
+
+    docs_file = Path("docs/conf.py")
+    old_contents = docs_file.read_text()
+    new_contents = re.sub(r"release\s+=\s+\"(.*)\"", f"release = \"{version}\"", old_contents)
+    docs_file.write_text(new_contents)
 
 if __name__ == '__main__':
     update_version()
