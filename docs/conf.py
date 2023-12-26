ac3548db098fd942dfd3bbc1950f0ace7f99070d
Paige Andrews <pmandrews@gmail.com>
2023-12-26 17:59:30 -0500

    Update docs release version manually

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2015-2023, Paige Andrews'
 author = 'Paige Andrews'
 
 # The full version, including alpha/beta/rc tags
-release = "2.0.0a1"
+release = "2.0.0"
 
 
 # -- General configuration ---------------------------------------------------
