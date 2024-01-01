9d8cac89ace7b9b19dad64be3c5b8942eacbb43c
Paige Andrews <pmandrews@gmail.com>
2024-01-01 17:07:49 -0500

    Add more upcoming icons

--- src/npc_gui/resources.py
+++ src/npc_gui/resources.py
@@ -6,7 +6,37 @@
 from PySide6 import QtCore
 
 qt_resource_data = b"\
-\x00\x00\x01\x90\
+\x00\x00\x01\xb7\
+<\
+svg class=\x22w-6 h\
+-6 text-gray-800\
+ dark:text-white\
+\x22 aria-hidden=\x22t\
+rue\x22 xmlns=\x22http\
+://www.w3.org/20\
+00/svg\x22 fill=\x22no\
+ne\x22 viewBox=\x220 0\
+ 20 20\x22>\x0d\x0a    <p\
+ath stroke=\x22curr\
+entColor\x22 stroke\
+-linecap=\x22round\x22\
+ stroke-linejoin\
+=\x22round\x22 stroke-\
+width=\x221.5\x22 d=\x22M\
+7 19H1.933A.97.9\
+7 0 0 1 1 18V5.8\
+28a2 2 0 0 1 .58\
+6-1.414l2.828-2.\
+828A2 2 0 0 1 5.\
+828 1h8.239A.97.\
+97 0 0 1 15 2v4M\
+6 1v4a1 1 0 0 1-\
+1 1H1m11 8h4m-2 \
+2v-4m5 2a5 5 0 1\
+ 1-10 0 5 5 0 0 \
+1 10 0Z\x22/>\x0d\x0a  </\
+svg>\x0d\x0a\
+\x00\x00\x01\x92\
 <\
 svg class=\x22w-6 h\
 -6 text-gray-800\
@@ -22,18 +52,48 @@ entColor\x22 stroke\
 -linecap=\x22round\x22\
  stroke-linejoin\
 =\x22round\x22 stroke-\
-width=\x222\x22 d=\x22M2.\
-539 17h12.476l4-\
-9H5m-2.461 9a1 1\
- 0 0 1-.914-1.40\
-6L5 8m-2.461 9H2\
-a1 1 0 0 1-1-1V2\
-a1 1 0 0 1 1-1h5\
-.443a1 1 0 0 1 .\
-8.4l2.7 3.6H16a1\
- 1 0 0 1 1 1v2H5\
-\x22/>\x0d\x0a  </svg>\x0d\x0a\
-\x00\x00\x01\x8c\
+width=\x221.5\x22 d=\x22M\
+2.539 17h12.476l\
+4-9H5m-2.461 9a1\
+ 1 0 0 1-.914-1.\
+406L5 8m-2.461 9\
+H2a1 1 0 0 1-1-1\
+V2a1 1 0 0 1 1-1\
+h5.443a1 1 0 0 1\
+ .8.4l2.7 3.6H16\
+a1 1 0 0 1 1 1v2\
+H5\x22/>\x0d\x0a  </svg>\x0d\
+\x0a\
+\x00\x00\x01\xa6\
+<\
+svg class=\x22w-6 h\
+-6 text-gray-800\
+ dark:text-white\
+\x22 aria-hidden=\x22t\
+rue\x22 xmlns=\x22http\
+://www.w3.org/20\
+00/svg\x22 fill=\x22no\
+ne\x22 viewBox=\x220 0\
+ 16 20\x22>\x0d\x0a    <p\
+ath stroke=\x22curr\
+entColor\x22 stroke\
+-linecap=\x22round\x22\
+ stroke-linejoin\
+=\x22round\x22 stroke-\
+width=\x221.5\x22 d=\x22M\
+6 1v4a1 1 0 0 1-\
+1 1H1m4 6 2 2 4-\
+4m4-8v16a.97.97 \
+0 0 1-.933 1H1.9\
+33A.97.97 0 0 1 \
+1 18V5.828a2 2 0\
+ 0 1 .586-1.414l\
+2.828-2.828A2 2 \
+0 0 1 5.828 1h8.\
+239A.97.97 0 0 1\
+ 15 2Z\x22/>\x0d\x0a  </s\
+vg>\x0d\x0a\
+\x00\x00\x01\x8e\
 <\
 svg class=\x22w-6 h\
 -6 text-gray-800\
@@ -49,18 +109,43 @@ entColor\x22 stroke\
 -linecap=\x22round\x22\
  stroke-linejoin\
 =\x22round\x22 stroke-\
-width=\x222\x22 d=\x22M7 \
-11h4m-2 2V9M2 5h\
-14a1 1 0 0 1 1 1\
-v10a1 1 0 0 1-1 \
-1H2a1 1 0 0 1-1-\
-1V6a1 1 0 0 1 1-\
-1Zm5.443-4H2a1 1\
- 0 0 0-1 1v3h9.9\
-43l-2.7-3.6a1 1 \
-0 0 0-.8-.4Z\x22/>\x0d\
+width=\x221.5\x22 d=\x22M\
+7 11h4m-2 2V9M2 \
+5h14a1 1 0 0 1 1\
+ 1v10a1 1 0 0 1-\
+1 1H2a1 1 0 0 1-\
+1-1V6a1 1 0 0 1 \
+1-1Zm5.443-4H2a1\
+ 1 0 0 0-1 1v3h9\
+.943l-2.7-3.6a1 \
+1 0 0 0-.8-.4Z\x22/\
+>\x0d\x0a  </svg>\x0d\x0a\
+\x00\x00\x01l\
+<\
+svg class=\x22w-6 h\
+-6 text-gray-800\
+ dark:text-white\
+\x22 aria-hidden=\x22t\
+rue\x22 xmlns=\x22http\
+://www.w3.org/20\
+00/svg\x22 fill=\x22no\
+ne\x22 viewBox=\x220 0\
+ 18 18\x22>\x0d\x0a    <p\
+ath stroke=\x22curr\
+entColor\x22 stroke\
+-linecap=\x22round\x22\
+ stroke-linejoin\
+=\x22round\x22 stroke-\
+width=\x221.5\x22 d=\x22M\
+1 5v11a1 1 0 0 0\
+ 1 1h14a1 1 0 0 \
+0 1-1V6a1 1 0 0 \
+0-1-1H1Zm0 0V2a1\
+ 1 0 0 1 1-1h5.4\
+43a1 1 0 0 1 .8.\
+4l2.7 3.6H1Z\x22/>\x0d\
 \x0a  </svg>\x0d\x0a\
-\x00\x00\x01j\
+\x00\x00\x01\x97\
 <\
 svg class=\x22w-6 h\
 -6 text-gray-800\
@@ -76,43 +161,18 @@ entColor\x22 stroke\
 -linecap=\x22round\x22\
  stroke-linejoin\
 =\x22round\x22 stroke-\
-width=\x222\x22 d=\x22M1 \
-5v11a1 1 0 0 0 1\
- 1h14a1 1 0 0 0 \
-1-1V6a1 1 0 0 0-\
-1-1H1Zm0 0V2a1 1\
- 0 0 1 1-1h5.443\
-a1 1 0 0 1 .8.4l\
-2.7 3.6H1Z\x22/>\x0d\x0a \
- </svg>\x0d\x0a\
-\x00\x00\x01\x95\
-<\
-svg class=\x22w-6 h\
--6 text-gray-800\
- dark:text-white\
-\x22 aria-hidden=\x22t\
-rue\x22 xmlns=\x22http\
-://www.w3.org/20\
-00/svg\x22 fill=\x22no\
-ne\x22 viewBox=\x220 0\
- 18 18\x22>\x0d\x0a    <p\
-ath stroke=\x22curr\
-entColor\x22 stroke\
--linecap=\x22round\x22\
- stroke-linejoin\
-=\x22round\x22 stroke-\
-width=\x222\x22 d=\x22M15\
- 11v4.833A1.166 \
-1.166 0 0 1 13.8\
-33 17H2.167A1.16\
-7 1.167 0 0 1 1 \
-15.833V4.167A1.1\
-66 1.166 0 0 1 2\
-.167 3h4.618m4.4\
-47-2H17v5.768M9.\
-111 8.889l7.778-\
-7.778\x22/>\x0d\x0a  </sv\
-g>\x0d\x0a\
+width=\x221.5\x22 d=\x22M\
+15 11v4.833A1.16\
+6 1.166 0 0 1 13\
+.833 17H2.167A1.\
+167 1.167 0 0 1 \
+1 15.833V4.167A1\
+.166 1.166 0 0 1\
+ 2.167 3h4.618m4\
+.447-2H17v5.768M\
+9.111 8.889l7.77\
+8-7.778\x22/>\x0d\x0a  </\
+svg>\x0d\x0a\
 "
 
 qt_resource_name = b"\
@@ -120,10 +180,19 @@ qt_resource_name = b"\
 \x00o\xa6S\
 \x00i\
 \x00c\x00o\x00n\x00s\
+\x00\x0c\
+\x09<\x9c\xa7\
+\x00d\
+\x00o\x00c\x00u\x00m\x00e\x00n\x00t\x00-\x00n\x00e\x00w\
 \x00\x0b\
 \x0c\x8a\xa3\xfe\
 \x00f\
 \x00o\x00l\x00d\x00e\x00r\x00-\x00o\x00p\x00e\x00n\
+\x00\x11\
+\x03\x08*%\
+\x00d\
+\x00o\x00c\x00u\x00m\x00e\x00n\x00t\x00-\x00v\x00a\x00l\x00i\x00d\x00a\x00t\x00e\
+\
 \x00\x0a\
 \x0a\xc8\xa8\x87\
 \x00f\
@@ -142,16 +211,20 @@ qt_resource_name = b"\
 qt_resource_struct = b"\
 \x00\x00\x00\x00\x00\x02\x00\x00\x00\x01\x00\x00\x00\x01\
 \x00\x00\x00\x00\x00\x00\x00\x00\
-\x00\x00\x00\x00\x00\x02\x00\x00\x00\x04\x00\x00\x00\x02\
+\x00\x00\x00\x00\x00\x02\x00\x00\x00\x06\x00\x00\x00\x02\
 \x00\x00\x00\x00\x00\x00\x00\x00\
-\x00\x00\x00X\x00\x00\x00\x00\x00\x01\x00\x00\x04\x92\
-\x00\x00\x01\x8c\xc6\x98\xec\xdc\
-\x00\x00\x00F\x00\x00\x00\x00\x00\x01\x00\x00\x03$\
-\x00\x00\x01\x8c\xc6\x93\xc6D\
-\x00\x00\x00,\x00\x00\x00\x00\x00\x01\x00\x00\x01\x94\
-\x00\x00\x01\x8c\xc6\x9f\xa2<\
+\x00\x00\x00J\x00\x00\x00\x00\x00\x01\x00\x00\x03Q\
+\x00\x00\x01\x8c\xc7\x10\xcc\xae\
+\x00\x00\x00\x9e\x00\x00\x00\x00\x00\x01\x00\x00\x07\xfd\
+\x00\x00\x01\x8c\xc7\x0f\xbfv\
+\x00\x00\x00\x8c\x00\x00\x00\x00\x00\x01\x00\x00\x06\x8d\
+\x00\x00\x01\x8c\xc7\x10\xdf?\
 \x00\x00\x00\x10\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00\
-\x00\x00\x01\x8c\xc6\x89\x09K\
+\x00\x00\x01\x8c\xc7\x10\xbf\xe7\
+\x00\x00\x00r\x00\x00\x00\x00\x00\x01\x00\x00\x04\xfb\
+\x00\x00\x01\x8c\xc7\x11`>\
+\x00\x00\x00.\x00\x00\x00\x00\x00\x01\x00\x00\x01\xbb\
+\x00\x00\x01\x8c\xc7\x10\xb0\xc6\
 "
 
 def qInitResources():
