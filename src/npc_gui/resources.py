6fe3319e9783f57aadc6052eccbe68763d64e455
Paige Andrews <pmandrews@gmail.com>
2024-01-01 14:40:29 -0500

    Add open folder icon

--- src/npc_gui/resources.py
+++ src/npc_gui/resources.py
@@ -5,9 +5,60 @@
 
 from PySide6 import QtCore
 
+qt_resource_data = b"\
+\x00\x00\x01\x90\
+<\
+svg class=\x22w-6 h\
+-6 text-gray-800\
+ dark:text-white\
+\x22 aria-hidden=\x22t\
+rue\x22 xmlns=\x22http\
+://www.w3.org/20\
+00/svg\x22 fill=\x22no\
+ne\x22 viewBox=\x220 0\
+ 21 18\x22>\x0d\x0a    <p\
+ath stroke=\x22curr\
+entColor\x22 stroke\
+-linecap=\x22round\x22\
+ stroke-linejoin\
+=\x22round\x22 stroke-\
+width=\x222\x22 d=\x22M2.\
+539 17h12.476l4-\
+9H5m-2.461 9a1 1\
+ 0 0 1-.914-1.40\
+6L5 8m-2.461 9H2\
+a1 1 0 0 1-1-1V2\
+a1 1 0 0 1 1-1h5\
+.443a1 1 0 0 1 .\
+8.4l2.7 3.6H16a1\
+ 1 0 0 1 1 1v2H5\
+\x22/>\x0d\x0a  </svg>\x0d\x0a\
+"
+
+qt_resource_name = b"\
+\x00\x05\
+\x00o\xa6S\
+\x00i\
+\x00c\x00o\x00n\x00s\
+\x00\x0b\
+\x0c\x8a\xa3\xfe\
+\x00f\
+\x00o\x00l\x00d\x00e\x00r\x00-\x00o\x00p\x00e\x00n\
+"
+
+qt_resource_struct = b"\
+\x00\x00\x00\x00\x00\x02\x00\x00\x00\x01\x00\x00\x00\x01\
+\x00\x00\x00\x00\x00\x00\x00\x00\
+\x00\x00\x00\x00\x00\x02\x00\x00\x00\x01\x00\x00\x00\x02\
+\x00\x00\x00\x00\x00\x00\x00\x00\
+\x00\x00\x00\x10\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00\
+\x00\x00\x01\x8c\xc6\x89\x09K\
+"
+
 def qInitResources():
     QtCore.qRegisterResourceData(0x03, qt_resource_struct, qt_resource_name, qt_resource_data)
 
 def qCleanupResources():
     QtCore.qUnregisterResourceData(0x03, qt_resource_struct, qt_resource_name, qt_resource_data)
 
+qInitResources()
