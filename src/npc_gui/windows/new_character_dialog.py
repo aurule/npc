bf9ec8ace0e6fa9a636a0420676193c21969486b
Paige Andrews <paige.andrews@modolabs.com>
2024-01-04 17:01:59 -0500

    sketch controls for new character dialog

--- src/npc_gui/windows/new_character_dialog.py
+++ src/npc_gui/windows/new_character_dialog.py
@@ -1,5 +1,5 @@
 from PySide6.QtWidgets import (
-    QDialog, QVBoxLayout, QDialogButtonBox
+    QDialog, QVBoxLayout, QDialogButtonBox, QFormLayout, QLabel, QPushButton, QLineEdit, QComboBox
 )
 
 class NewCharacterDialog(QDialog):
@@ -8,7 +8,34 @@ class NewCharacterDialog(QDialog):
 
         self.setWindowTitle("Add a New Character")
 
+        form_lines = QFormLayout()
+
+        # type picker
+        self.type_picker = QComboBox()
+        self.type_picker.setPlaceholderText("Pick a character type")
+        self.type_picker.setCurrentIndex(-1)
+        form_lines.addRow("&Type:", self.type_picker)
+
+        # name input
+        name_input = QLineEdit()
+        form_lines.addRow("&Name:", name_input)
+
+        # mnemonic input
+        mnemonic_input = QLineEdit()
+        form_lines.addRow("&Mnemonic:", mnemonic_input)
+
         master_layout = QVBoxLayout(self)
+        master_layout.addLayout(form_lines)
+
+        # label showing constructed path within characters path
+
+        tags_label = QLabel("Tags:")
+        master_layout.addWidget(tags_label)
+
+        # hbox with tag picker and value input, then small add/plus button
+        #   orrrrrr just use the table editing features to add rows
+        # small table of tags added so far
+        #   each tag name and value is editable
 
         QBtn = QDialogButtonBox.Save | QDialogButtonBox.Cancel
         buttonBox = QDialogButtonBox(QBtn)
