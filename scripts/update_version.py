52f80dfc56fbfc6380677e0c3b994f87e5a14936
Paige Andrews <pmandrews@gmail.com>
2024-01-06 14:09:43 -0500

    Update version script to handle placeholder in yaml defs

--- scripts/update_version.py
+++ scripts/update_version.py
@@ -3,6 +3,29 @@ import re
 import os
 from pathlib import Path
 
+def handle_placeholder(
+    root_dir: Path,
+    version: str,
+    excludes: list[str] = None,
+    ):
+    excludes = excludes or []
+
+    for root, dirs, files in os.walk(root_dir):
+        root = Path(root)
+
+        for name in files:
+            file_path = root / name
+            if any([file_path.match(pattern) for pattern in excludes]):
+                continue
+
+            try:
+                old_contents = file_path.read_text()
+            except:
+                continue
+            new_contents = re.sub(r"NEW_VERSION", version, old_contents)
+            if new_contents != old_contents:
+                file_path.write_text(new_contents)
+
 @click.command
 @click.option(
     "-v", "--version",
@@ -11,11 +34,13 @@ from pathlib import Path
     type=str,
     )
 def update_version(version):
+    # handle version file
     version_file = Path("src/npc/__init__.py")
     old_contents = version_file.read_text()
     new_contents = re.sub(r"version__\s+=\s+\"(.*)\"", f"version__ = \"{version}\"", old_contents)
     version_file.write_text(new_contents)
 
+    # handle docs
     docs_root = Path("docs")
 
     docs_conf = docs_root / "conf.py"
@@ -23,20 +48,17 @@ def update_version(version):
     new_contents = re.sub(r"release\s+=\s+\"(.*)\"", f"release = \"{version}\"", old_contents)
     docs_conf.write_text(new_contents)
 
-    doc_excludes = ["_build", "source"]
-    for root, dirs, files in os.walk(docs_root):
-        dirs = filter(lambda d: d not in doc_excludes, dirs)
-        root = Path(root)
+    handle_placeholder(docs_root, version, [])
 
-        for name in files:
-            file_path = root / name
-            try:
-                old_contents = file_path.read_text()
-            except:
-                continue
-            new_contents = re.sub(r"NEW_VERSION", version, old_contents)
-            if new_contents != old_contents:
-                file_path.write_text(new_contents)
+    # handle defs
+    handle_placeholder(
+        Path("src/npc/settings/systems"),
+        version,
+        ["*.py"])
+    handle_placeholder(
+        Path("src/npc/settings/types"),
+        version,
+        ["*.py"])
 
 if __name__ == '__main__':
     update_version()
