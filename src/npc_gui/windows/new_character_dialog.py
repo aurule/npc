e5e6541c1ea6d8a4db71a8752e9bfd35e205f9df
Paige Andrews <paige.andrews@modolabs.com>
2024-01-10 14:40:21 -0500

    Flesh out character dialog tag input

--- src/npc_gui/windows/new_character_dialog.py
+++ src/npc_gui/windows/new_character_dialog.py
@@ -1,10 +1,22 @@
 from PySide6.QtWidgets import (
-    QDialog, QVBoxLayout, QDialogButtonBox, QFormLayout, QLabel, QPushButton, QLineEdit, QComboBox
+    QDialog, QVBoxLayout, QDialogButtonBox, QFormLayout, QLabel, QPushButton, QLineEdit, QComboBox,
+    QHBoxLayout, QToolButton
 )
+from PySide6.QtCore import Qt
+
+from ..helpers import theme_or_resource_icon
+from ..widgets.size_policies import *
 
 class NewCharacterDialog(QDialog):
     def __init__(self, parent):
         super().__init__(parent=parent)
+
+        self.character_type = ""
+        self.character_name = ""
+        self.character_mnemonic = ""
+        self.charcter_tags = [] # list of RawTag objects
+
+        # tag defs view
 
         self.setWindowTitle("Add a New Character")
 
@@ -17,25 +29,44 @@ class NewCharacterDialog(QDialog):
         form_lines.addRow("&Type:", self.type_picker)
 
         # name input
-        name_input = QLineEdit()
-        form_lines.addRow("&Name:", name_input)
+        self.name_input = QLineEdit()
+        form_lines.addRow("&Name:", self.name_input)
 
         # mnemonic input
-        mnemonic_input = QLineEdit()
-        form_lines.addRow("&Mnemonic:", mnemonic_input)
+        self.mnemonic_input = QLineEdit()
+        form_lines.addRow("&Mnemonic:", self.mnemonic_input)
+
+        # path label
+        self.path_preview = QLabel("Characters/Test Mann - testy boi.npc")
+        self.path_preview.setSizePolicy(fixed_vertical)
+        form_lines.addRow("Path:", self.path_preview)
 
         master_layout = QVBoxLayout(self)
         master_layout.addLayout(form_lines)
 
-        # label showing constructed path within characters path
-
         tags_label = QLabel("Tags:")
+        tags_label.setSizePolicy(fixed_vertical)
         master_layout.addWidget(tags_label)
 
-        # hbox with tag picker and value input, then small add/plus button
-        #   orrrrrr just use the table editing features to add rows
-        # small table of tags added so far
-        #   each tag name and value is editable
+        # need a scrolling area
+        # either a standalone scroll area, or a flat group box
+
+        new_tag_line = QHBoxLayout()
+        tag_name_picker = QComboBox() # allow custom input
+        tag_name_picker.setPlaceholderText("Tag name")
+        tag_name_picker.setCurrentIndex(-1)
+        tag_name_picker.setEditable(True)
+        tag_name_picker.setSizePolicy(fixed_horizontal)
+        new_tag_line.addWidget(tag_name_picker)
+        tag_value_entry = QLineEdit()
+        new_tag_line.addWidget(tag_value_entry)
+        new_tag_btn = QToolButton()
+        new_tag_btn.setSizePolicy(fixed_horizontal)
+        new_tag_btn.setIcon(theme_or_resource_icon("list-add"))
+        new_tag_btn.setToolButtonStyle(Qt.ToolButtonIconOnly)
+        new_tag_btn.setToolTip("Add tag")
+        new_tag_line.addWidget(new_tag_btn)
+        master_layout.addLayout(new_tag_line)
 
         QBtn = QDialogButtonBox.Save | QDialogButtonBox.Cancel
         buttonBox = QDialogButtonBox(QBtn)
