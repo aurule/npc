0770eb45059e8dff44d55ace86728c5d835cde6f
Paige Andrews <pmandrews@gmail.com>
2024-01-28 23:09:30 -0500

    Get tag name completion working

--- src/npc_gui/models/tag_items_model.py
+++ src/npc_gui/models/tag_items_model.py
@@ -1,17 +1,25 @@
 from PySide6.QtCore import QAbstractListModel, Qt
 
+from npc.characters import Character
+
 class TagItemsModel(QAbstractListModel):
     def __init__(self, campaign):
         super().__init__()
 
-        self._data = list(campaign.tags.values())
+        excluded_tags = list(Character.MAPPED_TAGS.keys())
+        self._data = [campaign.get_tag(tag_name) for tag_name in sorted(campaign.tags.keys()) if tag_name not in excluded_tags]
 
     def data(self, index, role: int):
         match role:
             case Qt.DisplayRole:
                 return self._data[index.row()].name
+            case Qt.EditRole:
+                return self._data[index.row()].name
             case Qt.UserRole:
                 return self._data[index.row()]
+            # case Qt.AccessibleDescriptionRole:
+            #     return "separator"
+            #     to add a fancy separator item
 
     def headerData(self, section: int, orientation, role: int):
         if role == Qt.DisplayRole:
