423f67bda92441390bc52e331b52403722d01296
Paige Andrews <pmandrews@gmail.com>
2024-02-05 21:34:02 -0500

    Add sequence to tags from files

--- src/npc/characters/character_tagger.py
+++ src/npc/characters/character_tagger.py
@@ -17,6 +17,21 @@ class CharacterTagger():
         self.character = character
         self.context_stack: list[Taggable] = [character]
         self.hidden: list[str] = list()
+        self._tag_seq = 0
+
+    @property
+    def tag_seq(self) -> int:
+        """Get the current tag value
+
+        This is an auto-incrementing number meant to be unique for all the tags related to a particular
+        character. Fetching the current value also increments it.
+
+        Returns:
+            int: Current sequence number
+        """
+        val = self._tag_seq
+        self._tag_seq = val + 1
+        return val
 
     def apply_tags(self, rawtags: list[RawTag]):
         """Apply a list of raw tags to our character
@@ -127,7 +142,11 @@ class CharacterTagger():
 
         tag_spec = self.get_tag_spec(rawtag.name)
 
-        tag = Tag(name = rawtag.name, value = rawtag.value)
+        tag = Tag(
+            name = rawtag.name,
+            value = rawtag.value,
+            sequence = self.tag_seq
+        )
         if hidden_value := self.tag_is_hidden(tag):
             tag.hidden = hidden_value
         tag.spec = tag_spec.in_context(self.context_stack[-1].name, UndefinedTagSpec(rawtag.name))
