b7438365eeef6bc390108b53df05cee7030ebdeb
Paige Andrews <paige.andrews@modolabs.com>
2024-03-13 16:42:05 -0400

    Test hiding subtags

--- tests/test_npc/test_characters/test_character_tagger/test_tag_is_hidden.py
+++ tests/test_npc/test_characters/test_character_tagger/test_tag_is_hidden.py
@@ -3,51 +3,65 @@ from npc.characters import Character, RawTag, Tag
 
 from npc.characters import CharacterTagger
 
-def test_true_if_all(tmp_campaign):
-    character = Character()
-    tagger = CharacterTagger(tmp_campaign, character)
-    tagger.hidden = ["thing>>all"]
-    tag = Tag(name="thing", value="maybe")
+class TestTopLevelTag:
+    def test_true_if_all(self, tmp_campaign):
+        character = Character()
+        tagger = CharacterTagger(tmp_campaign, character)
+        tagger.hidden = ["thing>>all"]
+        tag = Tag(name="thing", value="maybe")
 
-    result = tagger.tag_is_hidden(tag)
+        result = tagger.tag_is_hidden(tag)
 
-    assert result == "all"
+        assert result == "all"
 
-def test_true_if_one(tmp_campaign):
-    character = Character()
-    tagger = CharacterTagger(tmp_campaign, character)
-    tagger.hidden = ["thing>>maybe"]
-    tag = Tag(name="thing", value="maybe")
+    def test_true_if_one(self, tmp_campaign):
+        character = Character()
+        tagger = CharacterTagger(tmp_campaign, character)
+        tagger.hidden = ["thing>>maybe"]
+        tag = Tag(name="thing", value="maybe")
 
-    result = tagger.tag_is_hidden(tag)
+        result = tagger.tag_is_hidden(tag)
 
-    assert result == "one"
+        assert result == "one"
 
-def test_false_if_wrong_value(tmp_campaign):
-    character = Character()
-    tagger = CharacterTagger(tmp_campaign, character)
-    tagger.hidden = ["thing>>other"]
-    tag = Tag(name="thing", value="maybe")
+    def test_false_if_wrong_value(self, tmp_campaign):
+        character = Character()
+        tagger = CharacterTagger(tmp_campaign, character)
+        tagger.hidden = ["thing>>other"]
+        tag = Tag(name="thing", value="maybe")
 
-    result = tagger.tag_is_hidden(tag)
+        result = tagger.tag_is_hidden(tag)
 
-    assert result is None
+        assert result is None
 
-def test_false_if_wrong_name(tmp_campaign):
-    character = Character()
-    tagger = CharacterTagger(tmp_campaign, character)
-    tagger.hidden = ["something>>all"]
-    tag = Tag(name="thing", value="maybe")
+    def test_false_if_wrong_name(self, tmp_campaign):
+        character = Character()
+        tagger = CharacterTagger(tmp_campaign, character)
+        tagger.hidden = ["something>>all"]
+        tag = Tag(name="thing", value="maybe")
 
-    result = tagger.tag_is_hidden(tag)
+        result = tagger.tag_is_hidden(tag)
 
-    assert result is None
+        assert result is None
 
-def test_false_if_nothing_hidden(tmp_campaign):
-    character = Character()
-    tagger = CharacterTagger(tmp_campaign, character)
-    tag = Tag(name="thing", value="maybe")
+    def test_false_if_nothing_hidden(self, tmp_campaign):
+        character = Character()
+        tagger = CharacterTagger(tmp_campaign, character)
+        tag = Tag(name="thing", value="maybe")
 
-    result = tagger.tag_is_hidden(tag)
+        result = tagger.tag_is_hidden(tag)
 
-    assert result is None
+        assert result is None
+
+class TestSubTag:
+    def test_works_on_subtags(self, tmp_campaign):
+        character = Character()
+        tagger = CharacterTagger(tmp_campaign, character)
+        tagger.hidden = ["location>>here>>wanderer>>all"]
+        parent_tag = Tag(name="location", value="here")
+        tagger.context_stack.append(parent_tag)
+        tag = Tag(name="wanderer")
+
+        result = tagger.tag_is_hidden(tag)
+
+        assert result == "all"
