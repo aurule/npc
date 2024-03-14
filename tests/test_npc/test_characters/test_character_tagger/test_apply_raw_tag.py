9f1f56e2f877c60fc8fd9c0681711cb92a0f867d
Paige Andrews <paige.andrews@modolabs.com>
2024-03-14 13:20:04 -0400

    Refactor tagger tests for metatags

--- tests/test_npc/test_characters/test_character_tagger/test_apply_raw_tag.py
+++ tests/test_npc/test_characters/test_character_tagger/test_apply_raw_tag.py
@@ -22,25 +22,149 @@ def test_makes_tag_for_mapped_when_instructed(tmp_campaign):
     assert character.realname != "Test Mann"
     assert character.tags[0].name == "realname"
 
-def test_handles_metatags(tmp_campaign):
-    new_defs = {
-        "metatags": {
-            "test": {
-                "desc": "A testing tag",
-                "static": {
-                    "foo": "bar"
+class TestHandlesMetatags:
+    """This incidentally tests the expand_metatag internal method"""
+
+    def test_creates_static_tags(self, tmp_campaign):
+        new_defs = {
+            "metatags": {
+                "test": {
+                    "desc": "A testing tag",
+                    "static": {
+                        "foo": "bar"
+                    }
                 }
             }
         }
-    }
-    tmp_campaign.patch_campaign_settings(new_defs)
-    character = Character()
-    tagger = CharacterTagger(tmp_campaign, character)
-    rawtag = RawTag("test", "nope")
+        tmp_campaign.patch_campaign_settings(new_defs)
+        character = Character()
+        tagger = CharacterTagger(tmp_campaign, character)
+        rawtag = RawTag("test", "nope")
 
-    tagger.apply_raw_tag(rawtag)
+        tagger.apply_raw_tag(rawtag)
 
-    assert character.tags[0].name == "foo"
+        assert character.tags[0].name == "foo"
+
+    def test_match_creates_known_tags(self, tmp_campaign):
+        new_defs = {
+            "metatags": {
+                "test": {
+                    "desc": "A testing tag",
+                    "static": {
+                        "foo": "bar"
+                    },
+                    "match": [
+                        "name"
+                    ]
+                }
+            }
+        }
+        tmp_campaign.patch_campaign_settings(new_defs)
+        character = Character()
+        tagger = CharacterTagger(tmp_campaign, character)
+        rawtag = RawTag("test", "nope")
+
+        tagger.apply_raw_tag(rawtag)
+
+        assert character.tags[1].name == "name"
+
+    def test_match_creates_unknown_tags(self, tmp_campaign):
+        new_defs = {
+            "metatags": {
+                "test": {
+                    "desc": "A testing tag",
+                    "static": {
+                        "foo": "bar"
+                    },
+                    "match": [
+                        "fimbul"
+                    ]
+                }
+            }
+        }
+        tmp_campaign.patch_campaign_settings(new_defs)
+        character = Character()
+        tagger = CharacterTagger(tmp_campaign, character)
+        rawtag = RawTag("test", "nope")
+
+        tagger.apply_raw_tag(rawtag)
+
+        assert character.tags[1].name == "fimbul"
+
+    def test_match_creates_known_subtags(self, tmp_campaign):
+        new_defs = {
+            "metatags": {
+                "test": {
+                    "desc": "A testing tag",
+                    "static": {
+                        "foo": "bar"
+                    },
+                    "match": [
+                        "name",
+                        "with"
+                    ]
+                }
+            }
+        }
+        tmp_campaign.patch_campaign_settings(new_defs)
+        character = Character()
+        tagger = CharacterTagger(tmp_campaign, character)
+        rawtag = RawTag("test", "nope alope")
+
+        tagger.apply_raw_tag(rawtag)
+
+        name_tag = character.tags[1]
+        assert name_tag.name == "name"
+        assert name_tag.subtags[0].name == "with"
+
+    def test_match_creates_subtags_then_main_tags(self, tmp_campaign):
+        new_defs = {
+            "metatags": {
+                "test": {
+                    "desc": "A testing tag",
+                    "static": {
+                        "foo": "bar"
+                    },
+                    "match": [
+                        "name",
+                        "with",
+                        "fimbul"
+                    ]
+                }
+            }
+        }
+        tmp_campaign.patch_campaign_settings(new_defs)
+        character = Character()
+        tagger = CharacterTagger(tmp_campaign, character)
+        rawtag = RawTag("test", "nope alope")
+
+        tagger.apply_raw_tag(rawtag)
+
+        assert character.tags[2].name == "fimbul"
+
+    def test_match_handles_incorrect_subtag(self, tmp_campaign):
+        new_defs = {
+            "metatags": {
+                "test": {
+                    "desc": "A testing tag",
+                    "static": {
+                        "foo": "bar"
+                    },
+                    "match": [
+                        "name",
+                        "role"
+                    ]
+                }
+            }
+        }
+        tmp_campaign.patch_campaign_settings(new_defs)
+        character = Character()
+        tagger = CharacterTagger(tmp_campaign, character)
+        rawtag = RawTag("test", "nope alope")
+
+        tagger.apply_raw_tag(rawtag)
+
+        assert character.tags[2].name == "role"
 
 def test_handles_regular_tags(tmp_campaign):
     character = Character()
