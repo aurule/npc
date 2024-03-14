723a5c19b7f72f4382c28f21c9e9ccb208f9bd91
Paige Andrews <paige.andrews@modolabs.com>
2024-03-14 16:29:38 -0400

    Test additional migration warnings

--- tests/test_npc/test_settings/test_migrations/test_migration_1to2/test_1to2_convert.py
+++ tests/test_npc/test_settings/test_migrations/test_migration_1to2/test_1to2_convert.py
@@ -24,3 +24,24 @@ def test_converts_keys(tmp_campaign):
 
     new_data = migration.load_settings("campaign")
     assert new_data.get("campaign.name") == "test"
+
+def test_warns_about_legacy_templates(tmp_campaign):
+    tmp_campaign.settings_file.unlink()
+    legacy_dir = tmp_campaign.root / ".npc" / "legacy"
+    legacy_dir.mkdir()
+    (legacy_dir / "listing").mkdir()
+    migration = Migration1to2(tmp_campaign.settings)
+    old_data = DataStore({"campaign_name": "test"})
+
+    result = migration.convert("campaign", old_data)
+
+    assert "Mako" in result[0].message
+
+def test_warns_about_old_hierarchy(tmp_campaign):
+    tmp_campaign.settings_file.unlink()
+    migration = Migration1to2(tmp_campaign.settings)
+    old_data = DataStore({"paths": {"hierarchy": "something"}})
+
+    result = migration.convert("campaign", old_data)
+
+    assert "subpath_components" in result[0].message
