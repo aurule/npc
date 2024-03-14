66e29ad5c3c7dc99c4f8250f371deed193b14f75
Paige Andrews <paige.andrews@modolabs.com>
2024-03-14 16:37:10 -0400

    Fully test character view class getter

--- tests/test_npc/test_views/test_get_view.py
+++ tests/test_npc/test_views/test_get_view.py
@@ -1,9 +1,19 @@
-from npc.views import get, CharacterView
+from npc.views import get, CharacterView, GroupView, TagView
 
-def test_returns_known_view_class():
+def test_returns_character_view():
     result = get("Character")
 
     assert result is CharacterView
+
+def test_returns_group_view():
+    result = get("Group")
+
+    assert result is GroupView
+
+def test_returns_tag_view():
+    result = get("Tag")
+
+    assert result is TagView
 
 def test_returns_unknown_none():
     result = get("nope")
